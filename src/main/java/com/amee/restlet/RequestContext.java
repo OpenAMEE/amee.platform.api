b712667cba9258afe592ee952dae51cc63530b13
Diggory <dig@amee.cc>
2010-03-11 22:38:33 +0000

    Initial version for PL-374.

---
+++ src/main/java/com/amee/restlet/RequestContext.java
@@ -0,0 +1,112 @@
+package com.amee.restlet;
+
+import org.apache.commons.lang.StringUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.restlet.data.Form;
+import org.restlet.data.Parameter;
+import org.restlet.data.Request;
+
+import java.util.Iterator;
+
+/**
+ * A simple bean for holding contextual information about the request.
+ * <p/>
+ * Its intended use is in debug statements etc.
+ */
+public class RequestContext {
+
+    private final Log log = LogFactory.getLog(getClass());
+    private final Log transactions = LogFactory.getLog("transactions");
+
+    private String requestPath = "";
+    private String method = "";
+    private String requestParameters = "";
+    private String error = "";
+    private String form = "";
+    private String label = "";
+    private String type = "";
+    private String more = "";
+    private long start = 0L;
+
+    public RequestContext() {
+        this.start = System.currentTimeMillis();
+    }
+
+    public void setMore(String more) {
+        if (StringUtils.isNotBlank(more)) {
+            this.more = more;
+        }
+    }
+
+    public void addMore(String more) {
+        if (StringUtils.isNotBlank(more)) {
+            if (StringUtils.isBlank(this.more)) {
+                this.more = more;
+            } else {
+                this.more = this.more + "|" + more;
+            }
+        }
+    }
+
+    public void setRequest(Request request) {
+        this.requestPath = request.getResourceRef().getPath();
+        this.method = request.getMethod().toString();
+        this.requestParameters = getParameters(request.getResourceRef().getQueryAsForm());
+    }
+
+    private String getParameters(Form parmameters) {
+        Iterator<Parameter> i = parmameters.iterator();
+        if (!i.hasNext())
+            return "";
+
+        StringBuilder sb = new StringBuilder();
+        for (; ;) {
+            Parameter p = i.next();
+            sb.append(p.getName());
+            sb.append("__");
+            if (!p.getName().equals("password")) {
+                sb.append(p.getValue());
+            } else {
+                sb.append("XXXXXX");
+            }
+            if (i.hasNext()) {
+                sb.append(", ");
+            } else {
+                return sb.toString();
+            }
+        }
+    }
+
+    public void setError(String error) {
+        if (StringUtils.isNotBlank(error)) {
+            this.error = error;
+        }
+    }
+
+    public void setForm(Form form) {
+        this.form = getParameters(form);
+    }
+
+    public void error() {
+        log.error(toString());
+    }
+
+    public void record() {
+        transactions.info(toString());
+    }
+
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(requestPath + "|");
+        sb.append(type + "|");
+        sb.append(label + "|");
+        sb.append(requestParameters.replace("=", "__") + "|");
+        sb.append(form + "|");
+        sb.append(error + "|");
+        sb.append(method + "|");
+        sb.append(more + "|");
+        sb.append(System.currentTimeMillis() - start);
+        return sb.toString();
+    }
+}
---
+++ src/main/java/com/amee/restlet/TransactionServerConverter.java
@@ -0,0 +1,23 @@
+package com.amee.restlet;
+
+import com.amee.base.TransactionController;
+import com.noelios.restlet.http.HttpResponse;
+import com.noelios.restlet.http.HttpServerConverter;
+import org.restlet.Context;
+
+public class TransactionServerConverter extends HttpServerConverter {
+
+    private TransactionController transactionController;
+
+    public TransactionServerConverter(Context context) {
+        super(context);
+        transactionController = (TransactionController) context.getAttributes().get("transactionController");
+    }
+
+    public void commit(HttpResponse response) {
+        // commit the response
+        super.commit(response);
+        // end transaction / entity manager
+        transactionController.afterCommit();
+    }
+}
