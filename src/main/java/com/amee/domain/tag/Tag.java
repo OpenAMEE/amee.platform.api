52e8183274847fd0efa556b7335ac8692dae68a6
Diggory Briercliffe <dig@amee.cc>
2010-05-17 14:10:45 +0100

    PL-221 - Added counts for Tags. Added Tag.entityTags to facilitate counting EntityTags per Tag. Removed caching from tag queries and introduced time-out. Tags now not duplicated. Added base ResourceException.

--- src/main/java/com/amee/domain/tag/Tag.java
+++ src/main/java/com/amee/domain/tag/Tag.java
@@ -6,9 +6,14 @@ import org.hibernate.annotations.Cache;
 import org.hibernate.annotations.CacheConcurrencyStrategy;
 import org.hibernate.annotations.Index;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import javax.persistence.Transient;
+import java.util.List;
 
 @Entity
 @Table(name = "TAG")
@@ -18,9 +23,17 @@ public class Tag extends AMEEEntity {
     public final static int TAG_MIN_SIZE = 3;
     public final static int TAG_MAX_SIZE = 255;
 
+    // Mapped for HQL. We don't use this collection Java-side.
+    @OneToMany(mappedBy = "tag", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @Cache(usage = CacheConcurrencyStrategy.NONSTRICT_READ_WRITE)
+    private List<EntityTag> entityTags;
+
     @Column(name = "TAG", nullable = false, length = TAG_MAX_SIZE)
     @Index(name = "TAG_IND")
     private String tag = "";
+
+    @Transient
+    private long count = 0;
 
     public Tag() {
         super();
@@ -49,5 +62,13 @@ public class Tag extends AMEEEntity {
             tag = "";
         }
         this.tag = tag;
+    }
+
+    public long getCount() {
+        return count;
+    }
+
+    public void setCount(long count) {
+        this.count = count;
     }
 }
