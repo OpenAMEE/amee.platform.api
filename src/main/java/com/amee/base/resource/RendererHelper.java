85032be6c7aca50ec35f6074b515cbdc9fd35608
Diggory Briercliffe <dig@amee.cc>
2010-06-07 18:49:16 +0100

    PL-1828 - Added pagination for search, categories and category items. RendererHelper will now always return a Renderer. Added set.default.MASTER_INDEX to wrapper.conf.

--- src/main/java/com/amee/base/resource/RendererHelper.java
+++ src/main/java/com/amee/base/resource/RendererHelper.java
@@ -6,24 +6,35 @@ import java.util.Map;
 public class RendererHelper<R> {
 
     public R getRenderer(RequestWrapper requestWrapper, Map<String, Class> renderers) {
-        try {
-            for (String acceptedMediaType : requestWrapper.getAcceptedMediaTypes()) {
-                if (renderers.containsKey(acceptedMediaType)) {
-                    return (R) renderers
-                            .get(acceptedMediaType)
+        R r = null;
+        if (!renderers.isEmpty()) {
+            try {
+                for (String acceptedMediaType : requestWrapper.getAcceptedMediaTypes()) {
+                    if (renderers.containsKey(acceptedMediaType)) {
+                        r = (R) renderers
+                                .get(acceptedMediaType)
+                                .getConstructor()
+                                .newInstance();
+                        break;
+                    }
+                }
+                if (r == null) {
+                    r = (R) renderers.values().toArray(new Class[0])[0]
                             .getConstructor()
                             .newInstance();
                 }
+            } catch (InstantiationException e) {
+                throw new RuntimeException("Caught InstantiationException: " + e.getMessage(), e);
+            } catch (IllegalAccessException e) {
+                throw new RuntimeException("Caught IllegalAccessException: " + e.getMessage(), e);
+            } catch (NoSuchMethodException e) {
+                throw new RuntimeException("Caught NoSuchMethodException: " + e.getMessage(), e);
+            } catch (InvocationTargetException e) {
+                throw new RuntimeException("Caught InvocationTargetException: " + e.getMessage(), e);
             }
-        } catch (InstantiationException e) {
-            throw new RuntimeException("Caught InstantiationException: " + e.getMessage(), e);
-        } catch (IllegalAccessException e) {
-            throw new RuntimeException("Caught IllegalAccessException: " + e.getMessage(), e);
-        } catch (NoSuchMethodException e) {
-            throw new RuntimeException("Caught NoSuchMethodException: " + e.getMessage(), e);
-        } catch (InvocationTargetException e) {
-            throw new RuntimeException("Caught InvocationTargetException: " + e.getMessage(), e);
+        } else {
+            throw new RuntimeException("No renderers defined.");
         }
-        return null;
+        return r;
     }
 }
