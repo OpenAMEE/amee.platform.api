7fe334f64da7d583dc4a56aa9d9cb58be298bdb3
Diggory Briercliffe <dig@amee.cc>
2010-07-29 17:23:59 +0100

    PL-2966 - Added BaseValidator to aid in validation via ValidationSpecifications. PL-2966 - Modified ValidationSpecification to provider a 'builder' style interface. PL-2962 - Modified ItemValueDefinitionValidator to use BaseValidator.

--- src/main/java/com/amee/platform/resource/itemvaluedefinition/ItemValueDefinitionValidator.java
+++ src/main/java/com/amee/platform/resource/itemvaluedefinition/ItemValueDefinitionValidator.java
@@ -1,64 +1,67 @@
 package com.amee.platform.resource.itemvaluedefinition;
 
+import com.amee.base.validation.BaseValidator;
 import com.amee.base.validation.ValidationSpecification;
 import com.amee.domain.data.ItemValueDefinition;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.Errors;
-import org.springframework.validation.Validator;
 
 @Service
 @Scope("prototype")
-public class ItemValueDefinitionValidator implements Validator {
+public class ItemValueDefinitionValidator extends BaseValidator {
 
     // Alpha numerics & underscore.
     private final static String PATH_PATTERN_STRING = "^[a-zA-Z0-9_]*$";
 
-    private ValidationSpecification nameSpec;
-    private ValidationSpecification pathSpec;
-    private ValidationSpecification wikiDocSpec;
-
     public ItemValueDefinitionValidator() {
         super();
-        // name
-        nameSpec = new ValidationSpecification();
-        nameSpec.setName("name");
-        nameSpec.setMinSize(ItemValueDefinition.NAME_MIN_SIZE);
-        nameSpec.setMaxSize(ItemValueDefinition.NAME_MAX_SIZE);
-        // path
-        pathSpec = new ValidationSpecification();
-        pathSpec.setName("path");
-        pathSpec.setMinSize(ItemValueDefinition.PATH_MIN_SIZE);
-        pathSpec.setMaxSize(ItemValueDefinition.PATH_MAX_SIZE);
-        pathSpec.setFormat(PATH_PATTERN_STRING);
-        pathSpec.setCustomValidation(new ValidationSpecification.CustomValidation() {
-            @Override
-            public int validate(Object o, Errors e) {
-                // Ensure ItemValueDefinition path is unique amongst peers. 
-                ItemValueDefinition itemValueDefinition = (ItemValueDefinition) o;
-                for (ItemValueDefinition ivd : itemValueDefinition.getItemDefinition().getItemValueDefinitions()) {
-                    if ((itemValueDefinition != ivd) && itemValueDefinition.getPath().equalsIgnoreCase(ivd.getPath())) {
-                        e.rejectValue("path", "duplicate");
-                        break;
-                    }
-                }
-                return ValidationSpecification.CONTINUE;
-            }
-        });
-        // wikiDoc
-        wikiDocSpec = new ValidationSpecification();
-        wikiDocSpec.setName("wikiDoc");
-        wikiDocSpec.setMaxSize(ItemValueDefinition.WIKI_DOC_MAX_SIZE);
-        wikiDocSpec.setAllowEmpty(true);
+        addName();
+        addPath();
+        addWikiDoc();
     }
 
     public boolean supports(Class clazz) {
         return ItemValueDefinition.class.isAssignableFrom(clazz);
     }
 
-    public void validate(Object o, Errors e) {
-        nameSpec.validate(o, e);
-        pathSpec.validate(o, e);
-        wikiDocSpec.validate(o, e);
+    private void addName() {
+        add(new ValidationSpecification()
+                .setName("name")
+                .setMinSize(ItemValueDefinition.NAME_MIN_SIZE)
+                .setMaxSize(ItemValueDefinition.NAME_MAX_SIZE)
+        );
+    }
+
+    private void addPath() {
+        add(new ValidationSpecification()
+                .setName("path")
+                .setMinSize(ItemValueDefinition.PATH_MIN_SIZE)
+                .setMaxSize(ItemValueDefinition.PATH_MAX_SIZE)
+                .setFormat(PATH_PATTERN_STRING)
+                .setCustomValidation(
+                new ValidationSpecification.CustomValidation() {
+                    @Override
+                    public int validate(Object o, Errors e) {
+                        // Ensure ItemValueDefinition path is unique amongst peers.
+                        ItemValueDefinition itemValueDefinition = (ItemValueDefinition) o;
+                        for (ItemValueDefinition ivd : itemValueDefinition.getItemDefinition().getItemValueDefinitions()) {
+                            if ((itemValueDefinition != ivd) && itemValueDefinition.getPath().equalsIgnoreCase(ivd.getPath())) {
+                                e.rejectValue("path", "duplicate");
+                                break;
+                            }
+                        }
+                        return ValidationSpecification.CONTINUE;
+                    }
+                })
+        );
+    }
+
+    private void addWikiDoc() {
+        add(new ValidationSpecification()
+                .setName("wikiDoc")
+                .setMaxSize(ItemValueDefinition.WIKI_DOC_MAX_SIZE)
+                .setAllowEmpty(true)
+        );
     }
 }
