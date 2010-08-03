68e66016e02ebb494d6f7e8193e6a39bfa8e6b2c
Diggory Briercliffe <dig@amee.cc>
2010-08-03 14:59:49 +0100

    PL-2963 - Validation and setting of ItemValueUsages in ItemValueDefinitions. Needed to enhance ValidationException to handle multiple ValidationResults.

--- src/main/java/com/amee/base/validation/ValidationException.java
+++ src/main/java/com/amee/base/validation/ValidationException.java
@@ -4,12 +4,16 @@ import com.amee.base.resource.ResourceException;
 import com.amee.base.resource.ValidationResult;
 import org.jdom.Document;
 import org.jdom.Element;
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ValidationException extends ResourceException {
 
-    private ValidationResult validationResult;
+    private List<ValidationResult> validationResults = new ArrayList<ValidationResult>();
 
     public ValidationException() {
         super();
@@ -20,12 +24,23 @@ public class ValidationException extends ResourceException {
         setValidationResult(validationResult);
     }
 
+    public ValidationException(List<ValidationResult> validationResults) {
+        this();
+        setValidationResults(validationResults);
+    }
+
     @Override
     public JSONObject getJSONObject() {
         try {
             JSONObject o = new JSONObject();
-            if (getValidationResult() != null) {
+            if (validationResults.size() == 1) {
                 o.put("validationResult", getValidationResult().getJSONObject());
+            } else if (!validationResults.isEmpty()) {
+                JSONArray arr = new JSONArray();
+                for (ValidationResult validationResult : validationResults) {
+                    arr.put(validationResult.getJSONObject());
+                }
+                o.put("validationResults", arr);
             }
             o.put("status", "INVALID");
             return o;
@@ -36,18 +51,30 @@ public class ValidationException extends ResourceException {
 
     public Document getDocument() {
         Element rootElem = new Element("Representation");
-        if (getValidationResult() != null) {
+        if (validationResults.size() == 1) {
             rootElem.addContent(getValidationResult().getElement());
+        } else if (!validationResults.isEmpty()) {
+            Element validationResultsElem = new Element("ValidationResults");
+            for (ValidationResult validationResult : validationResults) {
+                validationResultsElem.addContent(validationResult.getElement());
+            }
+            rootElem.addContent(validationResultsElem);
         }
         rootElem.addContent(new Element("Status").setText("INVALID"));
         return new Document(rootElem);
     }
 
     public ValidationResult getValidationResult() {
-        return validationResult;
+        return validationResults.isEmpty() ? null : validationResults.get(0);
     }
 
     public void setValidationResult(ValidationResult validationResult) {
-        this.validationResult = validationResult;
+        validationResults.clear();
+        validationResults.add(validationResult);
+    }
+
+    public void setValidationResults(List<ValidationResult> validationResults) {
+        validationResults.clear();
+        validationResults.addAll(validationResults);
     }
 }
