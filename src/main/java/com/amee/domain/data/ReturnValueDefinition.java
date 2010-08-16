ecc81342fa75ddefefb5651b3d68050bc2542ab4
David Keen <david.keen@amee.com>
2010-08-16 16:34:17 +0100

    Added TYPE to ReturnValueDefinition. PL-1892.

--- src/main/java/com/amee/domain/data/ReturnValueDefinition.java
+++ src/main/java/com/amee/domain/data/ReturnValueDefinition.java
@@ -7,6 +7,7 @@ import com.amee.domain.ValueDefinition;
 import com.amee.platform.science.AmountCompoundUnit;
 import com.amee.platform.science.AmountPerUnit;
 import com.amee.platform.science.AmountUnit;
+import org.hibernate.annotations.Cache;
 import org.hibernate.annotations.CacheConcurrencyStrategy;
 import org.springframework.beans.factory.annotation.Autowire;
 import org.springframework.beans.factory.annotation.Configurable;
@@ -20,9 +21,16 @@ import javax.persistence.Table;
 
 @Entity
 @Table(name = "RETURN_VALUE_DEFINITION")
-@org.hibernate.annotations.Cache(usage = CacheConcurrencyStrategy.NONSTRICT_READ_WRITE)
+@Cache(usage = CacheConcurrencyStrategy.NONSTRICT_READ_WRITE)
 @Configurable(autowire = Autowire.BY_TYPE)
 public class ReturnValueDefinition extends AMEEEnvironmentEntity {
+
+    public static final int TYPE_MIN_SIZE = 1;
+    public static final int TYPE_MAX_SIZE = 255;
+    public static final int UNIT_MIN_SIZE = 1;
+    public static final int UNIT_MAX_SIZE = 255;
+    public static final int PER_UNIT_MIN_SIZE = 1;
+    public static final int PER_UNIT_MAX_SIZE = 255;
 
     @ManyToOne(fetch = FetchType.LAZY, optional = true)
     @JoinColumn(name = "ITEM_DEFINITION_ID")
@@ -31,6 +39,9 @@ public class ReturnValueDefinition extends AMEEEnvironmentEntity {
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
     @JoinColumn(name = "VALUE_DEFINITION_ID")
     private ValueDefinition valueDefinition;
+
+    @Column(name = "TYPE")
+    private String type;
 
     @Column(name = "UNIT")
     private String unit;
@@ -49,6 +60,14 @@ public class ReturnValueDefinition extends AMEEEnvironmentEntity {
         super(itemDefinition.getEnvironment());
         setItemDefinition(itemDefinition);
         itemDefinition.add(this);
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
     }
 
     public AmountUnit getUnit() {
