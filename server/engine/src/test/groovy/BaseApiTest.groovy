f9cd536442e4a5333586f9fd6bebc79290795d25
Diggory Briercliffe <dig@amee.cc>
2010-08-18 00:06:13 +0100

    PL-3248 -   Create integration tests for updating an Item Definition in API V3.

--- server/engine/src/test/groovy/BaseApiTest.groovy
+++ server/engine/src/test/groovy/BaseApiTest.groovy
@@ -11,61 +11,77 @@ import org.springframework.context.support.ClassPathXmlApplicationContext
  */
 abstract class BaseApiTest {
 
-    static def context
-    static def container
+  static def context
+  static def container
 
-    def client
+  def client
 
-    // See import.sql
-    def uids = ['CD310BEBAC52', 'BBA3AC3E795E', '427DFCC65E52', '3FE23FDC8CEA', 'F27BF795BB04', '54C8A44254AA', '75AD9B83B7BF', '319DDB5EC18E', '4BD595E1873A', '3C03A03B5F3A',
-        '99B121BB416C', '066196F049DD', 'E71CA2FCFFEA', 'AA59F9613F2A', 'D9289C55E595', '3035D381872B']
+  // See import.sql
+  def uids = ['CD310BEBAC52', 'BBA3AC3E795E', '427DFCC65E52', '3FE23FDC8CEA', 'F27BF795BB04', '54C8A44254AA', '75AD9B83B7BF', '319DDB5EC18E', '4BD595E1873A', '3C03A03B5F3A',
+          '99B121BB416C', '066196F049DD', 'E71CA2FCFFEA', 'AA59F9613F2A', 'D9289C55E595', '3035D381872B']
 
-    def names = ['Root', 'Home', 'Appliances', 'Computers', 'Generic', 'Cooking', 'Entertainment', 'Generic', 'Kitchen', 'Generic',
-        'Business', 'Energy', 'Electricity', 'US', 'Subregion', 'Waste']
+  def names = ['Root', 'Home', 'Appliances', 'Computers', 'Generic', 'Cooking', 'Entertainment', 'Generic', 'Kitchen', 'Generic',
+          'Business', 'Energy', 'Electricity', 'US', 'Subregion', 'Waste']
 
-    def wikiNames = ['Root', 'Home', 'Appliances', 'Computers', 'Computers_generic', 'Cooking', 'Entertainment', 'Entertainment_generic', 'Kitchen', 'Kitchen_generic',
-        'Business', 'Business_energy', 'Electricity_by_Country', 'Energy_US', 'US_Egrid', 'Waste']
+  def wikiNames = ['Root', 'Home', 'Appliances', 'Computers', 'Computers_generic', 'Cooking', 'Entertainment', 'Entertainment_generic', 'Kitchen', 'Kitchen_generic',
+          'Business', 'Business_energy', 'Electricity_by_Country', 'Energy_US', 'US_Egrid', 'Waste']
 
-    @BeforeClass
-    static void start() {
+  @BeforeClass
+  static void start() {
 
-        // Spring application context
-        // TODO: Use spring annotation to load this?
-        context = new ClassPathXmlApplicationContext("classpath*:applicationContext*.xml")
+    addRandomStringMethodToString();
 
-        // Configure Restlet server (ajp, http, etc).
-        // TODO: Try and do this in Spring XML config.
-        def server = context.getBean("platformServer")
-        def transactionController = context.getBean("transactionController")
-        server.context.attributes.transactionController = transactionController // used in TransactionServerConverter
+    // Spring application context
+    // TODO: Use spring annotation to load this?
+    context = new ClassPathXmlApplicationContext("classpath*:applicationContext*.xml")
 
-        // TODO: Start this before all integration tests. exec-maven-plugin?
-        // Start the restlet container
-        container = context.getBean("platformContainer")
+    // Configure Restlet server (ajp, http, etc).
+    // TODO: Try and do this in Spring XML config.
+    def server = context.getBean("platformServer")
+    def transactionController = context.getBean("transactionController")
+    server.context.attributes.transactionController = transactionController // used in TransactionServerConverter
 
-        println "Starting container..."
-        container.start()
+    // TODO: Start this before all integration tests. exec-maven-plugin?
+    // Start the restlet container
+    container = context.getBean("platformContainer")
 
-        // Wait 5 seconds to allow the search index to be built (there must be a better way to do this?)
-        sleep(5000)
+    println "Starting container..."
+    container.start()
+
+    // Wait 5 seconds to allow the search index to be built (there must be a better way to do this?)
+    sleep(5000)
+  }
+
+  @AfterClass
+  static void stop() {
+    try {
+      println "Stopping container..."
+      container.stop()
+    } catch (e) {
+      // Do nothing
     }
+  }
 
-    @AfterClass
-    static void stop() {
-        try {
-            println "Stopping container..."
-            container.stop()
-        } catch (e) {
-            // Do nothing
-        }
+  @Before
+  void setUp() {
+
+    // Get the HTTP client
+    def config = new ConfigSlurper().parse(context.getResource('classpath:api.properties').getURL())
+    client = new RESTClient("http://${config.api.host}:${config.api.port}")
+    client.auth.basic config.api.user, config.api.password
+  }
+
+  // Add a random character generator to the String class.
+
+  static void addRandomStringMethodToString() {
+    String.metaClass.'static'.randomString = { length ->
+      // The chars used for the random string.
+      def list = ('a'..'z') + ('A'..'Z') + ('0'..'9');
+      // Make sure the list is long enough.
+      list = list * (1 + length / list.size());
+      // Shuffle it up good.
+      Collections.shuffle(list);
+      length > 0 ? list[0..length - 1].join() : '';
     }
-
-    @Before
-    void setUp() {
-
-        // Get the HTTP client
-        def config = new ConfigSlurper().parse(context.getResource('classpath:api.properties').getURL())
-        client = new RESTClient("http://${config.api.host}:${config.api.port}")
-        client.auth.basic config.api.user, config.api.password
-    }
+  }
 }
--- server/engine/src/test/groovy/ItemDefinitionIT.groovy
+++ server/engine/src/test/groovy/ItemDefinitionIT.groovy
@@ -1,3 +1,4 @@
+import groovyx.net.http.HttpResponseException
 import org.junit.Ignore
 import org.junit.Test
 import static groovyx.net.http.ContentType.*
@@ -22,8 +23,9 @@ class ItemDefinitionIT extends BaseApiTest {
 
   @Test
   void getItemDefinitionJson() {
-    client.contentType = JSON;
-    def response = client.get(path: '/3.1/definitions/11D3548466F2;full');
+    def response = client.get(
+            path: '/3.1/definitions/11D3548466F2;full',
+            contentType: JSON);
     assertEquals 200, response.status;
     assertEquals 'application/json', response.contentType;
     assertTrue response.data instanceof net.sf.json.JSON;
@@ -37,8 +39,9 @@ class ItemDefinitionIT extends BaseApiTest {
 
   @Test
   void getItemDefinitionXml() {
-    client.contentType = XML
-    def response = client.get(path: '/3.1/definitions/11D3548466F2;full');
+    def response = client.get(
+            path: '/3.1/definitions/11D3548466F2;full',
+            contentType: XML);
     assertEquals 200, response.status;
     assertEquals 'application/xml', response.contentType;
     assertEquals 'OK', response.data.Status.text();
@@ -46,7 +49,69 @@ class ItemDefinitionIT extends BaseApiTest {
     assertEquals 'device,rating', response.data.ItemDefinition.DrillDown.text();
     def allUsages = response.data.ItemDefinition.Usages.Usage;
     assertEquals 2, allUsages.size();
-    assert ['usage1', 'usage2'] == allUsages.Name*.text();
-    assert ['false', 'true'] == allUsages.@present*.text();
+    assertTrue(['usage1', 'usage2'] == allUsages.Name*.text());
+    assertTrue(['false', 'true'] == allUsages.@present*.text());
+  }
+
+  @Test
+  void updateItemDefinition() {
+    // 1) Do the update.
+    def responsePut = client.put(
+            path: '/3.1/definitions/11D3548466F2',
+            body: ['name': 'newName',
+                    'drillDown': 'newDrillDownA,newDrillDownB',
+                    'usages': 'usage1,usage2,usage3'],
+            requestContentType: URLENC,
+            contentType: JSON);
+    assertEquals 201, responsePut.status;
+    // 2) Check values have been updated.
+    client.contentType = JSON;
+    def responseGet = client.get(path: '/3.1/definitions/11D3548466F2;full');
+    assertEquals 200, responseGet.status;
+    assertEquals 'application/json', responseGet.contentType;
+    assertTrue responseGet.data instanceof net.sf.json.JSON;
+    assertEquals 'OK', responseGet.data.status;
+    assertEquals 'newName', responseGet.data.itemDefinition.name;
+    assertEquals 'newDrillDownA,newDrillDownB', responseGet.data.itemDefinition.drillDown;
+    assertEquals 3, responseGet.data.itemDefinition.usages.size();
+    assertTrue(['usage1', 'usage2', 'usage3'] == responseGet.data.itemDefinition.usages.collect {it.name});
+    assertTrue(['false', 'true', 'true'] == responseGet.data.itemDefinition.usages.collect {it.present});
+  }
+
+  @Test
+  void updateItemDefinitionNameEmpty() {
+    updateItemDefinitionField('name', 'empty', '');
+  }
+
+  @Test
+  void updateItemDefinitionNameShort() {
+    updateItemDefinitionField('name', 'short', 'a');
+  }
+
+  @Test
+  void updateItemDefinitionNameLong() {
+    updateItemDefinitionField('name', 'long', String.randomString(256));
+  }
+
+  void updateItemDefinitionField(field, code, value) {
+    try {
+      def body = [:];
+      body[field] = value;
+      client.put(
+              path: '/3.1/definitions/BB33FDB20228',
+              body: body,
+              requestContentType: URLENC,
+              contentType: JSON);
+      fail 'Response status code should have been 400.';
+    } catch (HttpResponseException e) {
+      def response = e.response;
+      assertEquals 400, response.status;
+      assertEquals 'application/json', response.contentType;
+      assertTrue response.data instanceof net.sf.json.JSON;
+      assertEquals 'INVALID', response.data.status;
+      assertTrue([field] == response.data.validationResult.errors.collect {it.field});
+      assertTrue([value] == response.data.validationResult.errors.collect {it.value});
+      assertTrue([code] == response.data.validationResult.errors.collect {it.code});
+    }
   }
 }
