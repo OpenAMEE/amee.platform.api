f803d8a16079dd21814c9401754eb21dcd57bac9
Diggory Briercliffe <dig@amee.cc>
2010-08-18 12:53:41 +0100

    PL-1892 - Added support for Location header to framework and used this in ReturnValueDefinitionsFormAcceptor. PL-1893 - Create RVD integration tests.

--- src/main/java/com/amee/base/resource/ResponseHelper.java
+++ src/main/java/com/amee/base/resource/ResponseHelper.java
@@ -8,10 +8,14 @@ import org.json.JSONObject;
 public abstract class ResponseHelper {
 
     public static Object getOK(RequestWrapper requestWrapper) {
+        return getOK(requestWrapper, null);
+    }
+
+    public static Object getOK(RequestWrapper requestWrapper, String location) {
         if (requestWrapper.getAcceptedMediaTypes().contains("application/json")) {
-            return getStatusJSONObject("OK");
+            return getStatusJSONObject("OK", location);
         } else {
-            return getStatusDocument("OK");
+            return getStatusDocument("OK", location);
         }
     }
 
@@ -24,9 +28,16 @@ public abstract class ResponseHelper {
     }
 
     public static JSONObject getStatusJSONObject(String status) {
+        return getStatusJSONObject(status, null);
+    }
+
+    public static JSONObject getStatusJSONObject(String status, String location) {
         try {
             JSONObject o = new JSONObject();
             o.put("status", status);
+            if (location != null) {
+                o.put("location", location);
+            }
             return o;
         } catch (JSONException e) {
             throw new RuntimeException("Caught JSONException: " + e.getMessage(), e);
@@ -34,8 +45,15 @@ public abstract class ResponseHelper {
     }
 
     public static Document getStatusDocument(String status) {
+        return getStatusDocument(status, null);
+    }
+
+    public static Document getStatusDocument(String status, String location) {
         Element rootElem = new Element("Representation");
         rootElem.addContent(new Element("Status").setText(status));
+        if (location != null) {
+            rootElem.addContent(new Element("Location").setText(location));
+        }
         return new Document(rootElem);
     }
 }
