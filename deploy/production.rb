5eac57c303aadd3b5630109fc6feb14caef1d672
Diggory Briercliffe <dig@amee.cc>
2010-09-02 11:40:06 +0100

    PL-3364 - Remove support for Data Category aliases (symlinks). Upgrades for domain, service and resource libs.

--- deploy/production.rb
+++ deploy/production.rb
@@ -1,8 +1,9 @@
 # The hosts where are are deploying 
 #role :app, "222403-app1.amee.com", "222404-app2.amee.com", "222405-app3.amee.com", "222406-app4.amee.com"
+role :app, "222403-app1.amee.com", "222404-app2.amee.com", "222406-app4.amee.com"
 #role :app, "222403-app1.amee.com"
 #role :app, "222404-app2.amee.com"
-role :app, "222405-app3.amee.com"
+#role :app, "222405-app3.amee.com"
 #role :app, "222406-app4.amee.com"
 #role :db,  "222407-db1.amee.com", "222409-db2.amee.com"
 
