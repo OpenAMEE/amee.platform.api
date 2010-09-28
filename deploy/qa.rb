5fed3a3c222837edc5319cd4e42efaea2fd831a8
Diggory Briercliffe <dig@amee.cc>
2010-09-28 10:40:49 +0100

    Deploy target for QA.

--- deploy/qa.rb
+++ deploy/qa.rb
@@ -1,3 +1,6 @@
-# The hosts where are are deploying 
-role :app, "platform-qa-1.amee.com", "platform-qa-2.amee.com"
-# role :db,  "db-qa.amee.com", :primary => true
+# The hosts where are are deploying
+role :app, "ec2-79-125-72-205.eu-west-1.compute.amazonaws.com"
+role :db,  "ec2-79-125-72-205.eu-west-1.compute.amazonaws.com", :primary => true
+
+set :application, "platform-api-qa"
+set :deploy_to, "/var/www/apps/#{application}"
