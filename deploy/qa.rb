cc58e0ba32d99cf2542c96536fe3b3d594999405
Diggory Briercliffe <dig@amee.cc>
2010-09-28 13:02:34 +0100

    Updated QA server address.

--- deploy/qa.rb
+++ deploy/qa.rb
@@ -1,6 +1,6 @@
 # The hosts where are are deploying
-role :app, "ec2-79-125-72-205.eu-west-1.compute.amazonaws.com"
-role :db,  "ec2-79-125-72-205.eu-west-1.compute.amazonaws.com", :primary => true
+role :app, "platform-qa.amee.com"
+role :db,  "platform-qa.amee.com", :primary => true
 
 set :application, "platform-api-qa"
 set :deploy_to, "/var/www/apps/#{application}"
