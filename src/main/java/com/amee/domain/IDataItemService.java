9b61541917dc73fac57f027d45c28213313ebc94
Diggory Briercliffe <dig@amee.cc>
2010-10-21 14:43:21 +0100

    PL-2789 - WIP.

--- src/main/java/com/amee/domain/IDataItemService.java
+++ src/main/java/com/amee/domain/IDataItemService.java
@@ -1,5 +1,6 @@
 package com.amee.domain;
 
+import com.amee.domain.data.DataCategory;
 import com.amee.domain.item.data.NuDataItem;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -7,11 +8,17 @@ import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 
 import java.util.Date;
+import java.util.List;
+import java.util.Set;
 
 public interface IDataItemService extends IItemService {
 
     // The UNIX time epoch, which is 1970-01-01 00:00:00. See: http://en.wikipedia.org/wiki/Unix_epoch
     public final static Date EPOCH = new Date(0);
+
+    public List<NuDataItem> getDataItems(DataCategory dataCategory);
+
+    public List<NuDataItem> getDataItems(Set<Long> dataItemIds);
 
     public NuDataItem getItemByUid(String uid);
 
@@ -21,17 +28,11 @@ public interface IDataItemService extends IItemService {
      * TODO: The following methods should live in a Renderer but are being
      * TODO: added here for convenience.
      */
-    public JSONObject getJSONObject(NuDataItem dataItem, boolean detailed,
-                                    boolean showHistory) throws JSONException;
-    
-    public JSONObject getJSONObject(NuDataItem dataItem, boolean detailed)
-            throws JSONException;
+    public JSONObject getJSONObject(NuDataItem dataItem, boolean detailed, boolean showHistory) throws JSONException;
 
-    public Element getElement(NuDataItem dataItem, Document document,
-                                    boolean detailed, boolean showHistory);
+    public JSONObject getJSONObject(NuDataItem dataItem, boolean detailed) throws JSONException;
 
-    public Element getElement(NuDataItem dataItem, Document document,
-                              boolean detailed);
+    public Element getElement(NuDataItem dataItem, Document document, boolean detailed, boolean showHistory);
 
-
+    public Element getElement(NuDataItem dataItem, Document document, boolean detailed);
 }
