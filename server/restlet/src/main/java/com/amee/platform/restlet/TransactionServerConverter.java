9bf8d38757eb3efb10e333aedbc9196fbd2b51de
Diggory Briercliffe <dig@amee.cc>
2010-11-03 08:34:59 +0000

    PL-6604 - Improve speed of instantiating entities by replacing Configurable annotation and related code with ThreadBeanLocal usage.

--- server/restlet/src/main/java/com/amee/platform/restlet/TransactionServerConverter.java
+++ server/restlet/src/main/java/com/amee/platform/restlet/TransactionServerConverter.java
@@ -7,19 +7,28 @@ import com.noelios.restlet.http.HttpResponse;
 import com.noelios.restlet.http.HttpServerCall;
 import com.noelios.restlet.http.HttpServerConverter;
 import org.restlet.Context;
+import org.springframework.context.ApplicationContext;
 
 public class TransactionServerConverter extends HttpServerConverter {
 
+    private ApplicationContext springContext;
     private TransactionController transactionController;
 
     public TransactionServerConverter(Context context) {
         super(context);
+        springContext = (ApplicationContext) context.getAttributes().get("springContext");
         transactionController = (TransactionController) context.getAttributes().get("transactionController");
     }
 
     public HttpRequest toRequest(HttpServerCall httpCall) {
         // Clear the ThreadBeanHolder at the start of each request.
         ThreadBeanHolder.clear();
+        // Store commonly used services.
+        ThreadBeanHolder.set("dataItemService", springContext.getBean("dataItemService"));
+        ThreadBeanHolder.set("profileItemService", springContext.getBean("profileItemService"));
+        ThreadBeanHolder.set("localeService", springContext.getBean("localeService"));
+        ThreadBeanHolder.set("metadataService", springContext.getBean("metadataService"));
+        ThreadBeanHolder.set("calculationService", springContext.getBean("calculationService"));
         // Pass request through.
         return super.toRequest(httpCall);
     }
