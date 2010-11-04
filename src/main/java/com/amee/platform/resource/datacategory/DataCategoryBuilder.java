a780c4081e5568547cbf128fb9e9c553eab8f7a9
Diggory Briercliffe <dig@amee.cc>
2010-11-04 10:55:04 +0000

    PL-3837 - V3 permissions - WIP.

--- src/main/java/com/amee/platform/resource/datacategory/DataCategoryBuilder.java
+++ src/main/java/com/amee/platform/resource/datacategory/DataCategoryBuilder.java
@@ -2,17 +2,29 @@ package com.amee.platform.resource.datacategory;
 
 import com.amee.base.resource.*;
 import com.amee.base.validation.ValidationException;
+import com.amee.domain.AMEEEntity;
+import com.amee.domain.IAMEEEntityReference;
+import com.amee.domain.auth.AccessSpecification;
+import com.amee.domain.auth.AuthorizationContext;
+import com.amee.domain.auth.PermissionEntry;
+import com.amee.domain.auth.User;
 import com.amee.domain.data.DataCategory;
 import com.amee.domain.data.ItemDefinition;
 import com.amee.domain.tag.Tag;
 import com.amee.platform.resource.EntityFilter;
 import com.amee.platform.resource.EntityFilterValidationHelper;
+import com.amee.service.auth.AuthenticationService;
+import com.amee.service.auth.AuthorizationService;
+import com.amee.service.auth.GroupService;
 import com.amee.service.data.DataService;
 import com.amee.service.tag.TagService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @Scope("prototype")
@@ -30,10 +42,24 @@ public class DataCategoryBuilder implements ResourceBuilder {
     @Autowired
     private RendererBeanFinder rendererBeanFinder;
 
+    @Autowired
+    private AuthenticationService authenticationService;
+
+    @Autowired
+    private AuthorizationService authorizationService;
+
+    @Autowired
+    private GroupService groupService;
+
+    private DataCategory dataCategory;
     private DataCategoryRenderer dataCategoryRenderer;
+    private User user;
+    private AuthorizationContext authorizationContext;
 
     @Transactional(readOnly = true)
     public Object handle(RequestWrapper requestWrapper) {
+        // Get the User.
+        user = authenticationService.getUserByUid(requestWrapper.getAttributes().get("activeUserUid"));
         // Get the DataCategory identifier.
         String dataCategoryIdentifier = requestWrapper.getAttributes().get("categoryIdentifier");
         if (dataCategoryIdentifier != null) {
@@ -42,14 +68,18 @@ public class DataCategoryBuilder implements ResourceBuilder {
             validationHelper.setEntityFilter(filter);
             if (validationHelper.isValid(requestWrapper.getQueryParameters())) {
                 // Get DataCategory (filter by status).
-                DataCategory dataCategory =
-                        dataService.getDataCategoryByIdentifier(dataCategoryIdentifier, filter.getStatus());
+                dataCategory = dataService.getDataCategoryByIdentifier(dataCategoryIdentifier, filter.getStatus());
                 if (dataCategory != null) {
-                    // Handle the DataCategory.
-                    this.handle(requestWrapper, dataCategory);
-                    DataCategoryRenderer renderer = getDataCategoryRenderer(requestWrapper);
-                    renderer.ok();
-                    return renderer.getObject();
+                    // Authorized?
+                    if (isAuthorized(getGetAccessSpecifications())) {
+                        // Handle the DataCategory.
+                        handleDataCategory(requestWrapper);
+                        DataCategoryRenderer renderer = getDataCategoryRenderer(requestWrapper);
+                        renderer.ok();
+                        return renderer.getObject();
+                    } else {
+                        throw new NotAuthorizedException();
+                    }
                 } else {
                     throw new NotFoundException();
                 }
@@ -61,9 +91,12 @@ public class DataCategoryBuilder implements ResourceBuilder {
         }
     }
 
-    public void handle(
-            RequestWrapper requestWrapper,
-            DataCategory dataCategory) {
+    public void handle(RequestWrapper requestWrapper, DataCategory dataCategory) {
+        this.dataCategory = dataCategory;
+        handleDataCategory(requestWrapper);
+    }
+
+    public void handleDataCategory(RequestWrapper requestWrapper) {
 
         DataCategoryRenderer renderer = getDataCategoryRenderer(requestWrapper);
         renderer.start();
@@ -115,10 +148,92 @@ public class DataCategoryBuilder implements ResourceBuilder {
 
     public DataCategoryRenderer getDataCategoryRenderer(RequestWrapper requestWrapper) {
         if (dataCategoryRenderer == null) {
-
-            // TODO: We should only render ecoinvent data as ecospold
             dataCategoryRenderer = (DataCategoryRenderer) rendererBeanFinder.getRenderer(DataCategoryRenderer.class, requestWrapper);
         }
         return dataCategoryRenderer;
+    }
+
+    /**
+     * Returns true if the request is authorized, otherwise false. AuthorizationService is used to
+     * do the authorization based on the supplied AccessSpecifications and the principals from GetPrincipals.
+     *
+     * @param accessSpecifications to use for authorization
+     * @return true if the request is authorized, otherwise false
+     */
+    public boolean isAuthorized(List<AccessSpecification> accessSpecifications) {
+        authorizationContext = new AuthorizationContext();
+        authorizationContext.addPrincipals(getPrincipals());
+        authorizationContext.addAccessSpecifications(accessSpecifications);
+        boolean authorized = authorizationService.isAuthorized(authorizationContext);
+        if (authorized && isRequireSuperUser() && !authorizationContext.isSuperUser()) {
+            authorized = false;
+        }
+        return authorized;
+    }
+
+    /**
+     * Returns a list of principals involved in authorization. Permissions from these principals will
+     * be compared against AccessSpecifications to determine if a request is authorized.
+     *
+     * @return a list of principals
+     */
+    public List<AMEEEntity> getPrincipals() {
+        List<AMEEEntity> principals = new ArrayList<AMEEEntity>();
+        principals.addAll(groupService.getGroupsForPrincipal(getActiveUser()));
+        principals.add(getActiveUser());
+        return principals;
+    }
+
+    /**
+     * Get the AccessSpecifications for GET requests. Creates an AccessSpecification for each entity
+     * from getEntities with VIEW as the PermissionEntry. This specifies that principals must have VIEW permissions
+     * for all the entities.
+     *
+     * @return AccessSpecifications for GET requests
+     */
+    public List<AccessSpecification> getGetAccessSpecifications() {
+        List<AccessSpecification> accessSpecifications = new ArrayList<AccessSpecification>();
+        for (IAMEEEntityReference entity : getDistinctEntities()) {
+            accessSpecifications.add(new AccessSpecification(entity, PermissionEntry.VIEW));
+        }
+        return accessSpecifications;
+    }
+
+    /**
+     * Returns a de-duped version of the list from getEntities().
+     *
+     * @return list of entities required for authorization
+     */
+    public List<IAMEEEntityReference> getDistinctEntities() {
+        List<IAMEEEntityReference> entities = new ArrayList<IAMEEEntityReference>();
+        for (IAMEEEntityReference entity : getEntities()) {
+            if (!entities.contains(entity)) {
+                entities.add(entity);
+            }
+        }
+        return entities;
+    }
+
+    /**
+     * Returns a list of entities required for authorization for the current resource. The list is
+     * in hierarchical order, from general to more specific (e.g., category -> sub-category -> item).
+     *
+     * @return list of entities required for authorization
+     */
+    public List<IAMEEEntityReference> getEntities() {
+        return dataCategory.getHierarchy();
+    }
+
+    /**
+     * Get the current active signed-in User.
+     *
+     * @return the current active signed-in User
+     */
+    public User getActiveUser() {
+        return user;
+    }
+
+    public boolean isRequireSuperUser() {
+        return false;
     }
 }
