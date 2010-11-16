e38cce5f592545e5b62248cd8acc66356744388f
Diggory Briercliffe <dig@amee.cc>
2010-11-16 15:50:09 +0000

    PL-8635 - Improved handling of Data Categories and Data Items. Attempt to fix duplication of Data Categories in results. Data Categories should now appear in relevance order.

--- src/main/java/com/amee/platform/resource/dataitem/DataItemsBuilder.java
+++ src/main/java/com/amee/platform/resource/dataitem/DataItemsBuilder.java
@@ -3,7 +3,6 @@ package com.amee.platform.resource.dataitem;
 import com.amee.base.domain.ResultsWrapper;
 import com.amee.base.resource.*;
 import com.amee.base.validation.ValidationException;
-import com.amee.domain.IAMEEEntity;
 import com.amee.domain.data.DataCategory;
 import com.amee.domain.data.DataItem;
 import com.amee.platform.search.DataItemsFilter;
@@ -78,10 +77,10 @@ public class DataItemsBuilder implements ResourceBuilder {
         DataItemsRenderer renderer = getDataItemsRenderer(requestWrapper);
         renderer.start();
         // Add Data Items to Renderer.
-        ResultsWrapper<IAMEEEntity> resultsWrapper = searchService.getDataItems(dataCategory, filter);
+        ResultsWrapper<DataItem> resultsWrapper = searchService.getDataItems(dataCategory, filter);
         renderer.setTruncated(resultsWrapper.isTruncated());
-        for (IAMEEEntity entity : resultsWrapper.getResults()) {
-            dataItemBuilder.handle(requestWrapper, (DataItem) entity);
+        for (DataItem dataItem : resultsWrapper.getResults()) {
+            dataItemBuilder.handle(requestWrapper, dataItem);
             renderer.newDataItem(dataItemBuilder.getDataItemRenderer(requestWrapper));
         }
     }
