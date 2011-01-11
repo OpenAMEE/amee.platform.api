af5c687d7adff8ea6ccd45a81365772edbd3930d
Diggory Briercliffe <dig@amee.cc>
2011-01-11 17:38:04 +0000

    PL-5531 - Drill down support in API V3.

--- server/engine/src/test/groovy/DrillDownIT.groovy
+++ server/engine/src/test/groovy/DrillDownIT.groovy
@@ -6,13 +6,22 @@ import static org.junit.Assert.assertTrue
 class DrillDownIT extends BaseApiTest {
 
   @Test
-  // @Ignore("Drill down SQL won't work in HSQLDB or H2")
-  void doDrillDownJson() {
-    client.contentType = JSON
-    def response = client.get(path: '/3.3/categories/Cooking/drill')
-    assertEquals 200, response.status
-    assertEquals 'application/json', response.contentType
-    assertTrue response.data instanceof net.sf.json.JSON
-    assertEquals 'OK', response.data.status
+  void canDrillDownJson() {
+    doDrillDownJson(['nothing_to_see': 'here'], 21, 'numberOfPeople');
+    doDrillDownJson(['numberOfPeople': '5'], 6, 'fuel');
+    doDrillDownJson(['numberOfPeople': '5', 'fuel': 'Gas'], 1, 'uid');
+  }
+
+  void doDrillDownJson(query, choicesSize, choicesName) {
+    client.contentType = JSON;
+    def response = client.get(
+            path: '/3.3/categories/Cooking/drill',
+            query: query);
+    assertEquals 200, response.status;
+    assertEquals 'application/json', response.contentType;
+    assertTrue response.data instanceof net.sf.json.JSON;
+    assertEquals 'OK', response.data.status;
+    assertEquals choicesSize, response.data.drill.choices.values.size();
+    assertEquals choicesName, response.data.drill.choices.name;
   }
 }
