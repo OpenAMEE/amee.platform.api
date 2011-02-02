2be575170210b1166b1b2f4c21c53f782d9c84c0
David Keen <david.keen@amee.com>
2011-02-02 16:43:19 +0000

    PL-9531 - Update tests for sorting of DataItems.

--- server/engine/src/test/groovy/DataItemIT.groovy
+++ server/engine/src/test/groovy/DataItemIT.groovy
@@ -10,13 +10,15 @@ import static org.junit.Assert.*
  */
 class DataItemIT extends BaseApiTest {
 
+    static def versions = [3.0, 3.1, 3.2]
+
   static def dataItemUids = [
+          'B6419AFB7114',
+          'B1EF6970E87C',
           '004CF30590A5',
-          '897513300787',
-          'ZZ7513300787',
-          '6C663D2B8681',
-          '9EFA0CE242D0',
-          'A81FD238C501'
+          'E011530FFEDC',
+          '471F553DF10A',
+          '9DD165D3AFC9'
   ].sort()
 
   static def oneGasItemValueValues = [
@@ -31,13 +33,13 @@ class DataItemIT extends BaseApiTest {
           'source',
           'fuel']
 
-  static def twoGasItemValueValues = [
-          '2',
-          '205',
+  static def tenElectricItemValueValues = [
+          '10',
+          '620',
           'BRE/MTP/dgen/defra 2007',
-          'Gas']
+          'Electric']
 
-  static def twoGasItemValuePaths = [
+  static def tenElectricItemValuePaths = [
           'numberOfPeople',
           'kgCO2PerYear',
           'source',
@@ -45,8 +47,12 @@ class DataItemIT extends BaseApiTest {
 
   @Test
   void getDataItemsJson() {
+      versions.each { version -> getDataItemsJson(version) }
+  }
+
+  def getDataItemsJson(version) {
     client.contentType = JSON
-    def response = client.get(path: '/3.1/categories/Cooking/items;full',
+    def response = client.get(path: "/${version}/categories/Cooking/items;full",
             query: ['resultLimit': '6'])
     assertEquals 200, response.status
     assertEquals 'application/json', response.contentType
@@ -56,12 +62,21 @@ class DataItemIT extends BaseApiTest {
     assertEquals dataItemUids.size(), response.data.items.size()
     def responseUids = response.data.items.collect { it.uid }.sort()
     assert dataItemUids == responseUids
+
+    // Should  be sorted by label
+    if (version >= 3.2 ) {
+        assertTrue response.data.items.first().label < response.data.items.last().label
+    }
   }
 
   @Test
   void getFilteredDataItemsJson() {
+      versions.each { version -> getFilteredDataItemsJson(version) }
+  }
+
+  def getFilteredDataItemsJson(version) {
     client.contentType = JSON
-    def response = client.get(path: '/3.1/categories/Cooking/items',
+    def response = client.get(path: "/${version}/categories/Cooking/items",
             query: ['numberOfPeople': '1'])
     assertEquals 200, response.status
     assertEquals 'application/json', response.contentType
@@ -69,12 +84,21 @@ class DataItemIT extends BaseApiTest {
     assertEquals 'OK', response.data.status
     assertFalse response.data.resultsTruncated
     assertEquals 5, response.data.items.size()
+
+    // Should not be sorted by label
+    if (version >= 3.2 ) {
+        assertFalse response.data.items.first().label < response.data.items.last().label
+    }
   }
 
   @Test
   void getDataItemsXml() {
+      versions.each { version -> getDataItemsXml(version) }
+  }
+
+  def getDataItemsXml(version) {
     client.contentType = XML
-    def response = client.get(path: '/3.1/categories/Cooking/items;full',
+    def response = client.get(path: "/${version}/categories/Cooking/items;full",
             query: ['resultLimit': '6'])
     assertEquals 200, response.status
     assertEquals 'application/xml', response.contentType
@@ -83,17 +107,28 @@ class DataItemIT extends BaseApiTest {
     def allDataItems = response.data.Items.Item
     assertEquals dataItemUids.size(), allDataItems.size()
     assert dataItemUids == allDataItems.@uid*.text().sort()
+
+    // Should  be sorted by label
+    if (version >= 3.2 ) {
+        assertTrue allDataItems[0].Label.text() < allDataItems[-1].Label.text()
+    }
   }
 
   @Test
   void getDataItemOneGasJson() {
+      versions.each { version -> getDataItemOneGasJson(version) }
+  }
+
+  def getDataItemOneGasJson(version) {
     client.contentType = JSON
-    def response = client.get(path: '/3.2/categories/Cooking/items/004CF30590A5;full');
+    def response = client.get(path: "/${version}/categories/Cooking/items/004CF30590A5;full");
     assertEquals 200, response.status;
     assertEquals 'application/json', response.contentType;
     assertTrue response.data instanceof net.sf.json.JSON;
     assertEquals 'OK', response.data.status;
-    assertEquals '1, Gas', response.data.item.label;
+    if (version >= 3.2) {
+        assertEquals '1, Gas', response.data.item.label;
+    }
     assertEquals '54C8A44254AA', response.data.item.categoryUid;
     assertEquals 'Cooking', response.data.item.categoryWikiName;
     assertEquals 'Cooking', response.data.item.itemDefinition.name;
@@ -105,12 +140,18 @@ class DataItemIT extends BaseApiTest {
 
   @Test
   void getDataItemOneGasXml() {
+      versions.each { version -> getDataItemOneGasXml(version) }
+  }
+
+  def getDataItemOneGasXml(version) {
     client.contentType = XML
-    def response = client.get(path: '/3.2/categories/Cooking/items/004CF30590A5;full');
+    def response = client.get(path: "/${version}/categories/Cooking/items/004CF30590A5;full");
     assertEquals 200, response.status;
     assertEquals 'application/xml', response.contentType;
     assertEquals 'OK', response.data.Status.text();
-    assertEquals '1, Gas', response.data.Item.Label.text();
+    if (version >= 3.2) {
+        assertEquals '1, Gas', response.data.Item.Label.text();
+    }
     assertEquals '54C8A44254AA', response.data.Item.CategoryUid.text();
     assertEquals 'Cooking', response.data.Item.CategoryWikiName.text();
     assertEquals 'Cooking', response.data.Item.ItemDefinition.Name.text();
@@ -122,46 +163,58 @@ class DataItemIT extends BaseApiTest {
   }
 
   @Test
-  void getDataItemTwoGasJson() {
+  void getDataItemTenElectricJson() {
+      versions.each { version -> getDataItemTenElectricJson(version) }
+  }
+
+  def getDataItemTenElectricJson(version) {
     client.contentType = JSON
-    def response = client.get(path: '/3.2/categories/Cooking/items/897513300787;full');
+    def response = client.get(path: "/${version}/categories/Cooking/items/9DD165D3AFC9;full");
     assertEquals 200, response.status;
     assertEquals 'application/json', response.contentType;
     assertTrue response.data instanceof net.sf.json.JSON;
     assertEquals 'OK', response.data.status;
-    assertEquals '2, Gas', response.data.item.label;
+    if (version >= 3.2) {
+        assertEquals '10, Electric', response.data.item.label;
+    }
     assertEquals '54C8A44254AA', response.data.item.categoryUid;
     assertEquals 'Cooking', response.data.item.categoryWikiName;
     assertEquals 'Cooking', response.data.item.itemDefinition.name;
-    assertEquals '/home/appliances/cooking/897513300787', response.data.item.fullPath;
-    assertEquals twoGasItemValueValues.size(), response.data.item.values.size();
-    assertTrue(twoGasItemValueValues == response.data.item.values.collect {it.value});
-    assertTrue(twoGasItemValuePaths == response.data.item.values.collect {it.path});
+    assertEquals '/home/appliances/cooking/9DD165D3AFC9', response.data.item.fullPath;
+    assertEquals tenElectricItemValueValues.size(), response.data.item.values.size();
+    assertTrue(tenElectricItemValueValues == response.data.item.values.collect {it.value});
+    assertTrue(tenElectricItemValuePaths == response.data.item.values.collect {it.path});
   }
 
   @Test
-  void getDataItemTwoGasXml() {
+  void getDataItemTenElectricXml() {
+      versions.each { version -> getDataItemTenElectricXml(version) }
+  }
+
+  def getDataItemTenElectricXml(version) {
     client.contentType = XML
-    def response = client.get(path: '/3.2/categories/Cooking/items/897513300787;full');
+    def response = client.get(path: "/${version}/categories/Cooking/items/9DD165D3AFC9;full");
     assertEquals 200, response.status;
     assertEquals 'application/xml', response.contentType;
     assertEquals 'OK', response.data.Status.text();
-    assertEquals '2, Gas', response.data.Item.Label.text();
+    if (version >= 3.2) {
+        assertEquals '10, Electric', response.data.Item.Label.text();
+    }
     assertEquals '54C8A44254AA', response.data.Item.CategoryUid.text();
     assertEquals 'Cooking', response.data.Item.CategoryWikiName.text();
     assertEquals 'Cooking', response.data.Item.ItemDefinition.Name.text();
-    assertEquals '/home/appliances/cooking/897513300787', response.data.Item.FullPath.text();
+    assertEquals '/home/appliances/cooking/9DD165D3AFC9', response.data.Item.FullPath.text();
     def allValues = response.data.Item.Values.Value;
-    assertEquals twoGasItemValueValues.size(), allValues.size();
-    assertTrue(twoGasItemValueValues == allValues.Value*.text());
-    assertTrue(twoGasItemValuePaths == allValues.Path*.text());
+    assertEquals tenElectricItemValueValues.size(), allValues.size();
+    assertTrue(tenElectricItemValueValues == allValues.Value*.text());
+    assertTrue(tenElectricItemValuePaths == allValues.Path*.text());
   }
 
   @Test
   void updateDataItemJson() {
     setAdminUser();
     def responsePut = client.put(
-            path: '/3.1/categories/Cooking/items/897513300787',
+            path: '/3.1/categories/Cooking/items/9DD165D3AFC9',
             body: ['name': 'newName',
                     'wikiDoc': 'wd',
                     'path': 'np',
@@ -170,7 +223,7 @@ class DataItemIT extends BaseApiTest {
             contentType: JSON);
     assertEquals 201, responsePut.status;
     def responseGet = client.get(
-            path: '/3.1/categories/Cooking/items/897513300787;full',
+            path: '/3.1/categories/Cooking/items/9DD165D3AFC9;full',
             contentType: JSON);
     assertEquals 200, responseGet.status;
     println responseGet.data;
@@ -184,7 +237,7 @@ class DataItemIT extends BaseApiTest {
   void updateDataItemUnauthorizedJson() {
     try {
       client.put(
-              path: '/3.1/categories/Cooking/items/897513300787',
+              path: '/3.1/categories/Cooking/items/9DD165D3AFC9',
               body: ['name': 'newName'],
               requestContentType: URLENC,
               contentType: JSON);
