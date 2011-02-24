04e6b5b413c0d20fa4feea63a845f1e5123a1185
David Keen <david.keen@amee.com>
2011-02-24 17:39:56 +0000

    PL-1815 - ItemDefinition acceptors and remover.

--- src/main/java/com/amee/platform/resource/itemdefinition/ItemDefinitionsResource.java
+++ src/main/java/com/amee/platform/resource/itemdefinition/ItemDefinitionsResource.java
@@ -1,17 +1,25 @@
 package com.amee.platform.resource.itemdefinition;
 
+import com.amee.base.resource.RequestWrapper;
+import com.amee.base.resource.ResourceAcceptor;
 import com.amee.base.resource.ResourceBuilder;
 import com.amee.base.resource.ResourceRenderer;
+import com.amee.base.validation.ValidationException;
 
 public interface ItemDefinitionsResource {
 
-    public static interface Builder extends ResourceBuilder {
+    interface Builder extends ResourceBuilder {
     }
 
-    public static interface Renderer extends ResourceRenderer {
+    interface Renderer extends ResourceRenderer {
 
         public void newItemDefinition(ItemDefinitionResource.Renderer renderer);
 
         public void setTruncated(boolean truncated);
+    }
+
+    interface FormAcceptor extends ResourceAcceptor {
+
+        public Object handle(RequestWrapper requestWrapper) throws ValidationException;
     }
 }
--- src/main/java/com/amee/platform/resource/itemdefinition/v_3_4/ItemDefinitionRemover_3_4_0.java
+++ src/main/java/com/amee/platform/resource/itemdefinition/v_3_4/ItemDefinitionRemover_3_4_0.java
@@ -1,4 +1,4 @@
-package com.amee.platform.resource.itemdefinition.v_3_1;
+package com.amee.platform.resource.itemdefinition.v_3_4;
 
 import com.amee.base.domain.Since;
 import com.amee.base.resource.MissingAttributeException;
@@ -8,10 +8,10 @@ import com.amee.base.resource.ResponseHelper;
 import com.amee.base.transaction.AMEETransaction;
 import com.amee.base.validation.ValidationException;
 import com.amee.domain.data.ItemDefinition;
-import com.amee.platform.resource.itemdefinition.ItemDefinitionAcceptor;
 import com.amee.platform.resource.itemdefinition.ItemDefinitionResource;
 import com.amee.service.auth.ResourceAuthorizationService;
 import com.amee.service.definition.DefinitionService;
+import com.amee.service.invalidation.InvalidationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
@@ -20,46 +20,36 @@ import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Scope("prototype")
-@Since("3.1.0")
-public class ItemDefinitionFormAcceptor_3_1_0 extends ItemDefinitionAcceptor implements ItemDefinitionResource.FormAcceptor {
+@Since("3.4.0")
+public class ItemDefinitionRemover_3_4_0 implements ItemDefinitionResource.Remover {
 
     @Autowired
-    protected DefinitionService definitionService;
+    private DefinitionService definitionService;
 
     @Autowired
     private ResourceAuthorizationService resourceAuthorizationService;
 
     @Override
     @AMEETransaction
-    @Transactional(propagation = Propagation.REQUIRED, rollbackFor = Exception.class)
-    public Object handle(RequestWrapper requestWrapper) throws ValidationException {
-        // Get ItemDefinition identifier.
+    @Transactional(propagation = Propagation.REQUIRED, rollbackFor = {ValidationException.class})
+    public Object handle(RequestWrapper requestWrapper) {
+
+        // Get ItemDefinition identifier
         String itemDefinitionIdentifier = requestWrapper.getAttributes().get("itemDefinitionIdentifier");
         if (itemDefinitionIdentifier != null) {
-            // Get ItemDefinition.
             ItemDefinition itemDefinition = definitionService.getItemDefinitionByUid(itemDefinitionIdentifier);
             if (itemDefinition != null) {
-                // Authorized?
-                resourceAuthorizationService.ensureAuthorizedForModify(
-                        requestWrapper.getAttributes().get("activeUserUid"), itemDefinition);
-                // Handle the ItemDefinition.
-                return handle(requestWrapper, itemDefinition);
+                resourceAuthorizationService.ensureAuthorizedForRemove(
+                    requestWrapper.getAttributes().get("activeUserUid"), itemDefinition);
+
+                definitionService.remove(itemDefinition);
+                definitionService.invalidate(itemDefinition);
+                return ResponseHelper.getOK(requestWrapper);
             } else {
                 throw new NotFoundException();
             }
         } else {
             throw new MissingAttributeException("itemDefinitionIdentifier");
-        }
-    }
-
-    protected Object handle(RequestWrapper requestWrapper, ItemDefinition itemDefinition) {
-        ItemDefinitionResource.ItemDefinitionValidator validator = getValidator(requestWrapper);
-        validator.setObject(itemDefinition);
-        if (validator.isValid(requestWrapper.getFormParameters())) {
-            definitionService.invalidate(itemDefinition);
-            return ResponseHelper.getOK(requestWrapper);
-        } else {
-            throw new ValidationException(validator.getValidationResult());
         }
     }
 }
