a9e340021117df06a74bdef9410074ec8cb005d1
Diggory Briercliffe <dig@amee.cc>
2011-02-24 15:05:48 +0000

    PL-9560 - Ensure integration tests cover all supported API V3 versions.

--- server/engine/src/test/groovy/DrillDownIT.groovy
+++ server/engine/src/test/groovy/DrillDownIT.groovy
@@ -5,23 +5,29 @@ import static org.junit.Assert.assertTrue
 
 class DrillDownIT extends BaseApiTest {
 
-  @Test
-  void canDrillDownJson() {
-    doDrillDownJson(['nothing_to_see': 'here'], 21, 'numberOfPeople');
-    doDrillDownJson(['numberOfPeople': '5'], 5, 'fuel');
-    doDrillDownJson(['numberOfPeople': '5', 'fuel': 'Gas'], 1, 'uid');
-  }
+    static def versions = [3.3]
 
-  void doDrillDownJson(query, choicesSize, choicesName) {
-    client.contentType = JSON;
-    def response = client.get(
-            path: '/3.3/categories/Cooking/drill',
-            query: query);
-    assertEquals 200, response.status;
-    assertEquals 'application/json', response.contentType;
-    assertTrue response.data instanceof net.sf.json.JSON;
-    assertEquals 'OK', response.data.status;
-    assertEquals choicesSize, response.data.drill.choices.values.size();
-    assertEquals choicesName, response.data.drill.choices.name;
-  }
+    @Test
+    void canDrillDownJson() {
+        doDrillDownJson(['nothing_to_see': 'here'], 21, 'numberOfPeople');
+        doDrillDownJson(['numberOfPeople': '5'], 5, 'fuel');
+        doDrillDownJson(['numberOfPeople': '5', 'fuel': 'Gas'], 1, 'uid');
+    }
+
+    def doDrillDownJson(query, choicesSize, choicesName) {
+        versions.each { version -> doDrillDownJson(query, choicesSize, choicesName, version) };
+    }
+
+    def doDrillDownJson(query, choicesSize, choicesName, version) {
+        client.contentType = JSON;
+        def response = client.get(
+                path: "/${version}/categories/Cooking/drill",
+                query: query);
+        assertEquals 200, response.status;
+        assertEquals 'application/json', response.contentType;
+        assertTrue response.data instanceof net.sf.json.JSON;
+        assertEquals 'OK', response.data.status;
+        assertEquals choicesSize, response.data.drill.choices.values.size();
+        assertEquals choicesName, response.data.drill.choices.name;
+    }
 }
