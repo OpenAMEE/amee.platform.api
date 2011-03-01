bbb098bf59fa71e0546c9dda59cf8ae64a0bcde2
Diggory Briercliffe <dig@amee.cc>
2011-03-01 14:21:00 +0000

    PL-10446 - Updated tests for startDate param.

--- server/engine/src/test/groovy/DataItemValueIT.groovy
+++ server/engine/src/test/groovy/DataItemValueIT.groovy
@@ -16,16 +16,29 @@ class DataItemValueIT extends BaseApiTest {
     }
 
     def getDataItemValuesJson(version) {
-        client.contentType = JSON;
+        getDataItemValuesJson(version, '289CCD5394AC', '0.81999', null); // Now.
+        getDataItemValuesJson(version, '289CCD5394AC', '0.81999', 'CURRENT'); // Now.
+        getDataItemValuesJson(version, '387C597FF2C4', '0.76426', '2002-08-01T00:00:00Z'); // A point in 2002.
+        getDataItemValuesJson(version, 'B3823E43A635', '0.8199856', 'FIRST'); // The unix EPOCH.
+        getDataItemValuesJson(version, '289CCD5394AC', '0.81999', 'LAST'); // The end of unix time.
+    }
+
+    def getDataItemValuesJson(version, uid, value, startDate) {
+        def query = [:];
+        if (startDate) {
+            query['startDate'] = startDate;
+        }
         def response = client.get(
-                path: "/${version}/categories/Greenhouse_Gas_Protocol_international_electricity/items/585E708CB4BE/values;full")
+                path: "/${version}/categories/Greenhouse_Gas_Protocol_international_electricity/items/585E708CB4BE/values;full",
+                query: query,
+                contentType: JSON);
         assertEquals 200, response.status;
         assertEquals 'application/json', response.contentType;
         assertTrue response.data instanceof net.sf.json.JSON;
         assertEquals 'OK', response.data.status;
         def values = response.data.values;
         assertEquals 3, values.size();
-        assert ['289CCD5394AC', '609405C3BC0C', '4097E4D3851A'].sort() == values.collect { it.uid }.sort();
-        assert ['0.81999', 'http://www.ghgprotocol.org/calculation-tools/all-tools', 'United Arab Emirates'].sort() == values.collect { it.value }.sort();
+        assert [uid, '609405C3BC0C', '4097E4D3851A'].sort() == values.collect { it.uid }.sort();
+        assert [value, 'http://www.ghgprotocol.org/calculation-tools/all-tools', 'United Arab Emirates'].sort() == values.collect { it.value }.sort();
     }
 }
