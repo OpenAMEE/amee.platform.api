6af6d54a2564e25b6416e5844f638753865622b7
Diggory Briercliffe <dig@amee.cc>
2011-03-10 16:00:57 +0000

    PL-10459 - Implement Algorithm support in V3 API.

--- server/engine/src/test/groovy/AlgorithmIT.groovy
+++ server/engine/src/test/groovy/AlgorithmIT.groovy
@@ -4,101 +4,128 @@ import static groovyx.net.http.ContentType.*
 import static org.junit.Assert.*
 
 /**
- * Tests for the Algorithm API.
+ * Tests for the Algorithm API. This API is available since version 3.4.0.
  */
 class AlgorithmIT extends BaseApiTest {
 
     static def versions = [3.4]
 
+    /**
+     * Tests for creation, fetch and deletion of an Algorithm using JSON responses.
+     *
+     * Create a new Algorithm by POSTing to '/definitions/{UID}'. The UID value is for the
+     * Item Definition the new Algorithm should belong to.
+     *
+     * Supported POST parameters are:
+     *
+     * <ul>
+     * <li>name - the name of the Algorithm.
+     * <li>content - the algorithm JavaScript content.
+     * </ul>
+     *
+     * NOTE: For detailed rules on these parameters see the validation tests below.
+     *
+     * Delete (TRASH) an Algorithm by sending a DELETE request to '/definitions/{UID}/algorithms/{UID}'. The
+     * first UID is for the Item Definition and the second is for the Algorithm.
+     */
     @Test
     void createDeleteAlgorithmJson() {
         versions.each { version -> createDeleteAlgorithmJson(version) }
     }
 
     def createDeleteAlgorithmJson(version) {
-        if (version >= 3.4) {
-            setAdminUser()
+        setAdminUser()
 
-            // Create a new Algorithm.
-            def responsePost = client.post(
-                    path: "/${version}/definitions/1B3B44CAE90C/algorithms",
-                    body: ['name': 'test',
-                            'content': 'xxx'],
-                    requestContentType: URLENC,
-                    contentType: JSON);
-            assertEquals 201, responsePost.status;
-            def location = responsePost.headers['Location'].value;
-            assertTrue location.startsWith("${config.api.protocol}://${config.api.host}");
+        // Create a new Algorithm.
+        def responsePost = client.post(
+                path: "/${version}/definitions/1B3B44CAE90C/algorithms",
+                body: ['name': 'test',
+                        'content': 'xxx'],
+                requestContentType: URLENC,
+                contentType: JSON);
+        assertEquals 201, responsePost.status;
+        def location = responsePost.headers['Location'].value;
+        assertTrue location.startsWith("${config.api.protocol}://${config.api.host}");
 
-            // Get the new Algorithm.
-            def responseGet = client.get(
-                    path: "${location};full",
-                    contentType: JSON);
-            assertEquals 200, responseGet.status;
-            assertEquals 'application/json', responseGet.contentType;
-            assertTrue responseGet.data instanceof net.sf.json.JSON;
-            assertEquals 'OK', responseGet.data.status;
-            assertEquals 'test', responseGet.data.algorithm.name;
-            assertEquals 'xxx', responseGet.data.algorithm.content;
+        // Get the new Algorithm.
+        def responseGet = client.get(
+                path: "${location};full",
+                contentType: JSON);
+        assertEquals 200, responseGet.status;
+        assertEquals 'application/json', responseGet.contentType;
+        assertTrue responseGet.data instanceof net.sf.json.JSON;
+        assertEquals 'OK', responseGet.data.status;
+        assertEquals 'test', responseGet.data.algorithm.name;
+        assertEquals 'xxx', responseGet.data.algorithm.content;
 
-            // Delete it
-            def responseDelete = client.delete(path: location)
-            assertEquals 200, responseDelete.status
+        // Delete it
+        def responseDelete = client.delete(path: location)
+        assertEquals 200, responseDelete.status
 
-            // Should get a 404 here
-            try {
-                client.get(path: location)
-                fail 'Should have thrown an exception'
-            } catch (HttpResponseException e) {
-                assertEquals 404, e.response.status
-            }
+        // Should get a 404 here
+        try {
+            client.get(path: location)
+            fail 'Should have thrown an exception'
+        } catch (HttpResponseException e) {
+            assertEquals 404, e.response.status
         }
     }
 
+    /**
+     * Tests for creation, fetch and deletion of an Algorithm using XML responses.
+     *
+     * See documentation for createDeleteAlgorithmJson above.
+     */
     @Test
     void createDeleteAlgorithmXml() {
         versions.each { version -> createDeleteAlgorithmXml(version) }
     }
 
     def createDeleteAlgorithmXml(version) {
-        if (version >= 3.4) {
-            setAdminUser()
+        setAdminUser()
 
-            // Create a new Algorithm.
-            def responsePost = client.post(
-                    path: "/${version}/definitions/11D3548466F2/algorithms",
-                    body: ['name': 'test',
-                            'content': 'xxx'],
-                    requestContentType: URLENC,
-                    contentType: XML)
-            assertEquals 201, responsePost.status
-            def location = responsePost.headers['Location'].value;
-            assertTrue location.startsWith("${config.api.protocol}://${config.api.host}")
+        // Create a new Algorithm.
+        def responsePost = client.post(
+                path: "/${version}/definitions/11D3548466F2/algorithms",
+                body: ['name': 'test',
+                        'content': 'xxx'],
+                requestContentType: URLENC,
+                contentType: XML)
+        assertEquals 201, responsePost.status
+        def location = responsePost.headers['Location'].value;
+        assertTrue location.startsWith("${config.api.protocol}://${config.api.host}")
 
-            // Get the new Algorithm.
-            def responseGet = client.get(
-                    path: "${location};full",
-                    contentType: XML)
-            assertEquals 200, responseGet.status
-            assertEquals 'application/xml', responseGet.contentType
-            assertEquals 'OK', responseGet.data.Status.text()
-            assertEquals 'test', responseGet.data.Algorithm.Name.text()
-            assertEquals 'xxx', responseGet.data.Algorithm.Content.text()
+        // Get the new Algorithm.
+        def responseGet = client.get(
+                path: "${location};full",
+                contentType: XML)
+        assertEquals 200, responseGet.status
+        assertEquals 'application/xml', responseGet.contentType
+        assertEquals 'OK', responseGet.data.Status.text()
+        assertEquals 'test', responseGet.data.Algorithm.Name.text()
+        assertEquals 'xxx', responseGet.data.Algorithm.Content.text()
 
-            // Delete it
-            def responseDelete = client.delete(path: location)
-            assertEquals 200, responseDelete.status
+        // Delete it
+        def responseDelete = client.delete(path: location)
+        assertEquals 200, responseDelete.status
 
-            // Should get a 404 here
-            try {
-                client.get(path: location)
-                fail 'Should have thrown an exception'
-            } catch (HttpResponseException e) {
-                assertEquals 404, e.response.status
-            }
+        // Should get a 404 here
+        try {
+            client.get(path: location)
+            fail 'Should have thrown an exception'
+        } catch (HttpResponseException e) {
+            assertEquals 404, e.response.status
         }
     }
 
+    /**
+     * Tests fetching a list of Algorithms using JSON responses.
+     *
+     * Fetch the Algorithm list by sending a GET request to '/definitions/{UID}/algorithms'. The UID value is for
+     * the Item Definition the Algorithms belong to.
+     *
+     * The same matrix parameters described in getAlgorithmJson are supported.
+     */
     @Test
     void getAlgorithmsJson() {
         versions.each { version -> getAlgorithmsJson(version) };
@@ -119,6 +146,11 @@ class AlgorithmIT extends BaseApiTest {
         }
     }
 
+    /**
+     * Tests fetching a list of Algorithms using XML responses.
+     *
+     * See documentation for getAlgorithmsJson above.
+     */
     @Test
     void getAlgorithmsXml() {
         versions.each { version -> getAlgorithmsXml(version) };
@@ -139,6 +171,21 @@ class AlgorithmIT extends BaseApiTest {
         }
     }
 
+    /**
+     * Tests fetching an Algorithm using JSON responses.
+     *
+     * Fetch an Algorithm by sending a GET request to '/definitions/{UID}/algorithms/{UID}'. The first UID is for
+     * the Item Definition and the second is for the Algorithm.
+     *
+     * Algorithm GET requests support the following matrix parameters to modify the response.
+     *
+     * <ul>
+     * <li>audit - include the status, created and modified values.
+     * <li>name - include the name.
+     * <li>content - include the JavaScript content.
+     * <li>itemDefinition - include the UID and name of the associated Item Definition (if linked).
+     * </ul>
+     */
     @Test
     void getAlgorithmJson() {
         versions.each { version -> getAlgorithmJson(version) }
@@ -157,6 +204,11 @@ class AlgorithmIT extends BaseApiTest {
         assertEquals 'Cooking', response.data.algorithm.itemDefinition.name;
     }
 
+    /**
+     * Tests fetching an Algorithm using XML responses.
+     *
+     * See documentation for getAlgorithmJson above.
+     */
     @Test
     void getAlgorithmXml() {
         versions.each { version -> getAlgorithmXml(version) }
@@ -174,6 +226,14 @@ class AlgorithmIT extends BaseApiTest {
         assertEquals 'Cooking', response.data.Algorithm.ItemDefinition.Name.text();
     }
 
+    /**
+     * Tests updating an Algorithm using JSON responses.
+     *
+     * Update an Algorithm by POSTing to '/definitions/{UID}/algorithms/{UID}'.  The first UID is for
+     * the Item Definition and the second is for the Algorithm.
+     *
+     * For supported parameters see documentation for createDeleteAlgorithmJson above.
+     */
     @Test
     void updateAlgorithmJson() {
         versions.each { version -> updateAlgorithmJson(version) };
@@ -201,6 +261,11 @@ class AlgorithmIT extends BaseApiTest {
         assertEquals 'New content JSON.', responseGet.data.algorithm.content;
     }
 
+    /**
+     * Tests updating an Algorithm using XML responses.
+     *
+     * See documentation for updateAlgorithmJson above.
+     */
     @Test
     void updateAlgorithmXml() {
         versions.each { version -> updateAlgorithmXml(version) }
@@ -227,11 +292,38 @@ class AlgorithmIT extends BaseApiTest {
         assertEquals 'New content XML.', responseGet.data.Algorithm.Content.text()
     }
 
+    /**
+     * Tests the validation rules for the Algorithm name field.
+     *
+     * The rules are as follows:
+     *
+     * <ul>
+     * <li>Mandatory.
+     * <li>Duplicates are allowed.
+     * <li>No longer than 255 chars.
+     * <li>Must not be empty.
+     * </ul>
+     */
     @Test
-    void updateInvalidAlgorithm() {
+    void updateWithInvalidName() {
         setAdminUser();
         updateAlgorithmFieldJson('name', 'empty', '');
         updateAlgorithmFieldJson('name', 'long', String.randomString(256));
+    }
+
+    /**
+     * Tests the validation rules for the Algorithm content field.
+     *
+     * The rules are as follows:
+     *
+     * <ul>
+     * <li>Optional.
+     * <li>No longer than 32767 chars.
+     * </ul>
+     */
+    @Test
+    void updateWithInvalidContent() {
+        setAdminUser();
         updateAlgorithmFieldJson('content', 'long', String.randomString(32768));
     }
 
