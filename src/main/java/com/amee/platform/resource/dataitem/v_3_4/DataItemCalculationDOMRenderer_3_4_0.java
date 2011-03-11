fce60a37945269a0552183e6fb4c0c3b583aa533
David Keen <david.keen@amee.com>
2011-03-11 16:30:05 +0000

    PL-9547 - Add xml representation to calculation resource.

--- src/main/java/com/amee/platform/resource/dataitem/v_3_4/DataItemCalculationDOMRenderer_3_4_0.java
+++ src/main/java/com/amee/platform/resource/dataitem/v_3_4/DataItemCalculationDOMRenderer_3_4_0.java
@@ -1,9 +1,13 @@
 package com.amee.platform.resource.dataitem.v_3_4;
 
 import com.amee.base.domain.Since;
+import com.amee.domain.data.ItemValueDefinition;
 import com.amee.domain.item.data.DataItem;
+import com.amee.domain.sheet.Choice;
 import com.amee.domain.sheet.Choices;
 import com.amee.platform.resource.dataitem.DataItemCalculationResource;
+import com.amee.platform.science.Note;
+import com.amee.platform.science.ReturnValue;
 import com.amee.platform.science.ReturnValues;
 import com.amee.service.item.DataItemService;
 import org.jdom.Document;
@@ -11,6 +15,8 @@ import org.jdom.Element;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
+
+import java.util.Map;
 
 @Service
 @Scope("prototype")
@@ -23,6 +29,8 @@ public class DataItemCalculationDOMRenderer_3_4_0 implements DataItemCalculation
     private DataItem dataItem;
     private Element rootElem;
     private Element returnValuesElem;
+    private Element notesElem;
+    private Element valuesElem;
 
     @Override
     public void start() {
@@ -41,10 +49,64 @@ public class DataItemCalculationDOMRenderer_3_4_0 implements DataItemCalculation
 
     @Override
     public void addReturnValues(ReturnValues returnValues) {
+        returnValuesElem = new Element("Amounts");
+
+        // Add the return values
+        for (Map.Entry<String, ReturnValue> entry : returnValues.getReturnValues().entrySet()) {
+            Element amountElem = new Element("Amount");
+            amountElem.setAttribute("type", entry.getValue().getType());
+            amountElem.setAttribute("unit", entry.getValue().getUnit());
+            amountElem.setAttribute("perUnit", entry.getValue().getPerUnit());
+            if (entry.getKey().equals(returnValues.getDefaultType())) {
+                amountElem.setAttribute("default", "true");
+            }
+            amountElem.setText(Double.toString(entry.getValue().getValue()));
+            returnValuesElem.addContent(amountElem);
+        }
+        if (returnValuesElem.getChildren().size() > 0) {
+            rootElem.addContent(returnValuesElem);
+        }
+
+        notesElem = new Element("Notes");
+
+        // Add the notes
+        for (Note note : returnValues.getNotes()) {
+            Element noteElem = new Element("Note");
+            noteElem.setAttribute("type", note.getType());
+            noteElem.setText(note.getValue());
+            notesElem.addContent(noteElem);
+        }
+        if (notesElem.getChildren().size() > 0) {
+            rootElem.addContent(notesElem);
+        }
     }
 
     @Override
     public void addValues(Choices values) {
+        valuesElem = new Element("Values");
+
+        // Add the supplied values
+        Map<String, ItemValueDefinition> itemValueDefinitions = dataItem.getItemDefinition().getItemValueDefinitionsMap();
+        for (Choice choice : values.getChoices()) {
+            Element valueElem = new Element("Value");
+            valueElem.setAttribute("name", choice.getName());
+            valueElem.setText(choice.getValue());
+
+            // Add details from the ItemValueDefinition.
+            ItemValueDefinition itemValueDefinition = itemValueDefinitions.get(choice.getName());
+            if (itemValueDefinition != null) {
+                if (itemValueDefinition.hasUnit()) {
+                    valueElem.setAttribute("unit", itemValueDefinition.getUnit().toString());
+                }
+                if (itemValueDefinition.hasPerUnit()) {
+                    valueElem.setAttribute("perUnit", itemValueDefinition.getPerUnit().toString());
+                }
+            }
+            valuesElem.addContent(valueElem);
+        }
+        if (valuesElem.getChildren().size() > 0) {
+            rootElem.addContent(valuesElem);
+        }
     }
 
     @Override
