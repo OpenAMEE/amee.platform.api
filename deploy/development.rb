c78bdff351e20e48ebabc57533ede9e124cbb68d
David Keen <david.keen@amee.com>
2011-04-15 13:57:22 +0100

    Update dev server name to monsoon. OPS-1250.

--- deploy/development.rb
+++ deploy/development.rb
@@ -1,6 +1,6 @@
 # The hosts where are are deploying 
-role :app, "flood.amee.com"
-role :db,  "flood.amee.com", :primary => true
+role :app, "monsoon.amee.com"
+role :db,  "monsoon.amee.com", :primary => true
 
 set :application, "platform-api-dev"
 set :deploy_to, "/var/www/apps/#{application}"
