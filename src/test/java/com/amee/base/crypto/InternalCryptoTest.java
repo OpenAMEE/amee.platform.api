f95fe54fabb2c11ddfa53eb1545e8db58e4a53d6
David Keen <david.keen@amee.com>
2011-10-04 10:06:18 +0100

    Uncommented crypto tests. PL-10424.

--- src/test/java/com/amee/base/crypto/InternalCryptoTest.java
+++ src/test/java/com/amee/base/crypto/InternalCryptoTest.java
@@ -1,6 +1,12 @@
 package com.amee.base.crypto;
 
+import org.junit.Before;
 import org.junit.Test;
+
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.spec.SecretKeySpec;
+
+import static org.junit.Assert.*;
 
 /**
  * TODO: The tests commented out below don't seem to work in Bamboo.
@@ -14,29 +20,29 @@ public class InternalCryptoTest {
 
     }
 
-//    @Before
-//    public void setUp() throws CryptoException {
-//        InternalCrypto.initialise((SecretKeySpec) InternalCrypto.getNewKey(), "1234123412341234".getBytes());
-//    }
-//
-//    @Test
-//    public void shouldEncryptAndDecrypt() throws CryptoException {
-//        String test = "hello";
-//        String encrypted = InternalCrypto.encrypt(test);
-//        String decrypted = InternalCrypto.decrypt(encrypted);
-//        assertEquals(test, decrypted);
-//    }
-//
-//    @Test
-//    public void shouldFailWithBadInput() throws CryptoException {
-//        String test = "hello";
-//        String encrypted = InternalCrypto.encrypt(test);
-//        encrypted = encrypted.substring(1);
-//        try {
-//            InternalCrypto.decrypt(encrypted);
-//            fail("Decryption should have thrown an IllegalBlockSizeException.");
-//        } catch (CryptoException e) {
-//            assertEquals(e.getCause().getClass(), IllegalBlockSizeException.class);
-//        }
-//    }
+    @Before
+    public void setUp() throws CryptoException {
+        InternalCrypto.initialise((SecretKeySpec) InternalCrypto.getNewKey(), "1234123412341234".getBytes());
+    }
+
+    @Test
+    public void shouldEncryptAndDecrypt() throws CryptoException {
+        String test = "hello";
+        String encrypted = InternalCrypto.encrypt(test);
+        String decrypted = InternalCrypto.decrypt(encrypted);
+        assertEquals(test, decrypted);
+    }
+
+    @Test
+    public void shouldFailWithBadInput() throws CryptoException {
+        String test = "hello";
+        String encrypted = InternalCrypto.encrypt(test);
+        encrypted = encrypted.substring(1);
+        try {
+            InternalCrypto.decrypt(encrypted);
+            fail("Decryption should have thrown an IllegalBlockSizeException.");
+        } catch (CryptoException e) {
+            assertEquals(e.getCause().getClass(), IllegalBlockSizeException.class);
+        }
+    }
 }
