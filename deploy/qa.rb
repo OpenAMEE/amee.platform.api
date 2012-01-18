9a55a624f94bf55bb800ce073c8b7f274936f0f4
David Keen <david.keen@amee.com>
2012-01-18 16:23:57 +0000

    Update qa capistrano deploy script. OPS-1943.

--- deploy/qa.rb
+++ deploy/qa.rb
@@ -1,6 +1,5 @@
 # The hosts where are are deploying
-role :app, "app3.amee.com"
-role :db,  "app3.amee.com", :primary => true
+role :app, "ec2-184-72-166-99.compute-1.amazonaws.com", "ec2-50-19-29-105.compute-1.amazonaws.com"
 
 set :application, "platform-api-qa"
 set :deploy_to, "/var/www/apps/#{application}"
