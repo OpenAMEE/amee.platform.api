393215f098893ab5ac8c4d837b8071d01b0cc607
David Keen <david.keen@amee.com>
2012-02-02 11:58:42 +0000

    Cleanup test. PL-11213.

--- server/engine/src/test/groovy/com/amee/integration/ProfileItemIT.groovy
+++ server/engine/src/test/groovy/com/amee/integration/ProfileItemIT.groovy
@@ -15,7 +15,7 @@ import org.joda.time.format.ISODateTimeFormat
 class ProfileItemIT extends BaseApiTest {
     
     // Days in a year
-    static def TYear = 365.242199
+    static def DAYS_IN_YEAR = 365.242199
 
     def cookingProfileUid = 'UCP4SKANF6CS'
     def cookingProfileItemUids = ['J7TICQCEMGEA', 'CR2IS4R423WK']
@@ -608,13 +608,17 @@ class ProfileItemIT extends BaseApiTest {
             }
         }
     }
-    
+
+    /**
+     * Tests calculations are performed correctly with different query ranges.
+     * This test taken from the v2 time_resolution_spec.rb test.
+     */
     @Test
-    void prorata() {
-        versions.each { version -> prorata(version) }
+    void timeSeries() {
+        versions.each { version -> timeSeries(version) }
     }
 
-    def prorata(version) {
+    def timeSeries(version) {
         if (version >= 3.6) {
             
             // cases relating to a single valued data series
@@ -622,68 +626,68 @@ class ProfileItemIT extends BaseApiTest {
             // cases relating to profile item within query range
             incase(version, 'WT0S', 1, 9, 2, 7, true, false, 2.0 * 23.0)
             incase(version, 'W00S', 1, 9, 2, 7, false, false, 2.0 * 23.0)
-            incase(version, 'WTRS', 1, 9, 2, 7, true, true, 2.0 * 23.0 * 5.0 / TYear)
+            incase(version, 'WTRS', 1, 9, 2, 7, true, true, 2.0 * 23.0 * 5.0 / DAYS_IN_YEAR)
             incase(version, 'W0RS', 1, 9, 2, 7, false, true, 2.0 * 23.0)
 
             incase(version, 'WT01', 1, 9, 2, 7, true, false, 2.0 * 23.0, false)
             incase(version, 'W001', 1, 9, 2, 7, false, false, 2.0 * 23.0, false)
-            incase(version, 'WTR1', 1, 9, 2, 7, true, true, 2.0 * 23.0 * 5.0 / TYear, false)
+            incase(version, 'WTR1', 1, 9, 2, 7, true, true, 2.0 * 23.0 * 5.0 / DAYS_IN_YEAR, false)
             incase(version, 'W0R1', 1, 9, 2, 7, false, true, 2.0 * 23.0, false)
 
             incase(version, 'WT0E', 41, 49, 42, 47, true, false, 17.0 * 23.0)
             incase(version, 'W00E', 41, 49, 42, 47, false, false, 17.0 * 23.0)
-            incase(version, 'WTRE', 41, 49, 42, 47, true, true, 17.0 * 23.0 * 5.0 / TYear)
+            incase(version, 'WTRE', 41, 49, 42, 47, true, true, 17.0 * 23.0 * 5.0 / DAYS_IN_YEAR)
             incase(version, 'W0RE', 41, 49, 42, 47, false, true, 17.0 * 23.0)
 
             // cases relating to profile item partially overlapping query range
             incase(version, 'PT0S', 1, 5, 2, 7, true, false, 2.0 * 23.0)
             incase(version, 'P00S', 1, 5, 2, 7, false, false, 2.0 * 23.0)
-            incase(version, 'PTRS', 1, 5, 2, 7, true, true, 2.0 * 23.0 * 3.0 / TYear)
+            incase(version, 'PTRS', 1, 5, 2, 7, true, true, 2.0 * 23.0 * 3.0 / DAYS_IN_YEAR)
             incase(version, 'P0RS', 1, 5, 2, 7, false, true, 2.0 * 23.0 * 3.0 / 5.0)
 
             incase(version, 'PT01', 1, 5, 2, 7, true, false, 2.0 * 23.0, false)
             incase(version, 'P001', 1, 5, 2, 7, false, false, 2.0 * 23.0, false)
-            incase(version, 'PTR1', 1, 5, 2, 7, true, true, 2.0 * 23.0 * 3.0 / TYear, false)
+            incase(version, 'PTR1', 1, 5, 2, 7, true, true, 2.0 * 23.0 * 3.0 / DAYS_IN_YEAR, false)
             incase(version, 'P0R1', 1, 5, 2, 7, false, true, 2.0 * 23.0 * 3.0 / 5.0, false)
 
             incase(version, 'PT0E', 41, 45, 42, 47, true, false, 17.0 * 23.0)
             incase(version, 'P00E', 41, 45, 42, 47, false, false, 17.0 * 23.0)
-            incase(version, 'PTRE', 41, 45, 42, 47, true, true, 17.0 * 23.0 * 3.0 / TYear)
+            incase(version, 'PTRE', 41, 45, 42, 47, true, true, 17.0 * 23.0 * 3.0 / DAYS_IN_YEAR)
             incase(version, 'P0RE', 41, 45, 42, 47, false, true, 17.0 * 23.0 * 3.0 / 5.0)
 
             // cases relating to profile item wholly overlapping query range
             incase(version, 'LT0S', 2, 7, 1, 9, true, false, 2.0 * 23.0)
             incase(version, 'L00S', 2, 7, 1, 9, false, false, 2.0 * 23.0)
-            incase(version, 'LTRSD', 2, 7, 1, 9, true, true, 2.0 * 23.0 * 5.0 / TYear)
+            incase(version, 'LTRSD', 2, 7, 1, 9, true, true, 2.0 * 23.0 * 5.0 / DAYS_IN_YEAR)
             incase(version, 'L0RS', 2, 7, 1, 9, false, true, 2.0 * 23.0 * 5.0 / 8.0)
 
             incase(version, 'LT01', 2, 7, 1, 9, true, false, 2.0 * 23.0, false)
             incase(version, 'L001', 2, 7, 1, 9, false, false, 2.0 * 23.0, false)
-            incase(version, 'LTR1D', 2, 7, 1, 9, true, true, 2.0 * 23.0 * 5.0 / TYear, false)
+            incase(version, 'LTR1D', 2, 7, 1, 9, true, true, 2.0 * 23.0 * 5.0 / DAYS_IN_YEAR, false)
             incase(version, 'L0R1', 2, 7, 1, 9, false, true, 2.0 * 23.0 * 5.0 / 8.0, false)
 
             incase(version, 'LT0E', 42, 47, 41, 49, true, false, 17.0 * 23.0)
             incase(version, 'L00E', 42, 47, 41, 49, false, false, 17.0 * 23.0)
-            incase(version, 'LTRED', 42, 47, 41, 49, true, true, 17.0 * 23.0 * 5.0 / TYear)
+            incase(version, 'LTRED', 42, 47, 41, 49, true, true, 17.0 * 23.0 * 5.0 / DAYS_IN_YEAR)
 
             incase(version, 'L0RE', 42, 47, 41, 49, false, true, 17.0 * 23.0 * 5.0 / 8.0)
 
             // cases relating to profile item without end partially overlapping range
             incase(version, 'ET0S', 1, 5, 2, null, true, false, 17.0 * 23.0)
             incase(version, 'E00S', 1, 5, 2, null, false, false, 17.0 * 23.0)
-            incase(version, 'ETRS', 1, 5, 2, null, true, true, 2.0 * 23.0 * 3.0 / TYear)
+            incase(version, 'ETRS', 1, 5, 2, null, true, true, 2.0 * 23.0 * 3.0 / DAYS_IN_YEAR)
             incase(version, 'E0RSD', 1, 5, 2, null, false, true, 2.0 * 23.0) // I (JH) disagree w spec
             // incase('E0RSD', 1, 5, 2, null, false, true, 0) // I think this
 
             incase(version, 'ET01', 1, 5, 2, null, true, false, 2.0 * 23.0, false)
             incase(version, 'E001', 1, 5, 2, null, false, false, 2.0 * 23.0, false)
-            incase(version, 'ETR1', 1, 5, 2, null, true, true, 2.0 * 23.0 * 3.0 / TYear, false)
+            incase(version, 'ETR1', 1, 5, 2, null, true, true, 2.0 * 23.0 * 3.0 / DAYS_IN_YEAR, false)
             incase(version, 'E0R1D', 1, 5, 2, null, false, true, 2.0 * 23.0, false) // I (JH) disagree w spec
             // incase('E0R1D', 1, 5, 2, null, false, true, 0, false, version)
 
             incase(version, 'ET0E', 41, 45, 42, null, true, false, 17.0 * 23.0)
             incase(version, 'E00E', 41, 45, 42, null, false, false, 17.0 * 23.0)
-            incase(version, 'ETRE', 41, 45, 42, null, true, true, 17.0 * 23.0 * 3.0 / TYear)
+            incase(version, 'ETRE', 41, 45, 42, null, true, true, 17.0 * 23.0 * 3.0 / DAYS_IN_YEAR)
             incase(version, 'E0RED', 41, 45, 42, null, false, true, 17.0 * 23.0) // I (JH) disagree w spec
             // incase('E0RED', 41, 45, 42, null, false, true, 0, version)
 
@@ -692,43 +696,129 @@ class ProfileItemIT extends BaseApiTest {
             // cases relating to profile item within query range
             incase(version, 'WT0M', 1, 39, 12, 27, true, false, 23.0 * (3.0 * 8.0 + 11.0 * 7.0) / 15.0)
             incase(version, 'W00M', 1, 39, 12, 27, false, false, 23.0 * (3.0 * 8.0 + 11.0 * 7.0) / 15.0)
-            incase(version, 'WTRM', 1, 39, 12, 27, true, true, 23.0 * (3.0 * 8.0 + 11.0 * 7.0) / TYear)
+            incase(version, 'WTRM', 1, 39, 12, 27, true, true, 23.0 * (3.0 * 8.0 + 11.0 * 7.0) / DAYS_IN_YEAR)
             incase(version, 'W0RM', 1, 39, 12, 27, false, true, 23.0 * (3.0 * 8.0 + 11.0 * 7.0) / 15.0)
 
             incase(version, 'WT0F', 1, 49, 12, 37, true, false, 23.0 * (3.0 * 8.0 + 11.0 * 10.0 + 13.0 * 7.0) / 25.0)
             incase(version, 'W00F', 1, 49, 12, 37, false, false, 23.0 * (3.0 * 8.0 + 11.0 * 10.0 + 13.0 * 7.0) / 25.0)
-            incase(version, 'WTRF', 1, 49, 12, 37, true, true, 23.0 * (3.0 * 8.0 + 11.0 * 10.0 + 13.0 * 7.0) / TYear)
+            incase(version, 'WTRF', 1, 49, 12, 37, true, true, 23.0 * (3.0 * 8.0 + 11.0 * 10.0 + 13.0 * 7.0) / DAYS_IN_YEAR)
             incase(version, 'W0RF', 1, 49, 12, 37, false, true, 23.0 * (3.0 * 8.0 + 11.0 * 10.0 + 13.0 * 7.0) / 25.0)
 
             // cases relating to profile item partially overlapping query range
             incase(version, 'PT0M', 1, 25, 12, 37, true, false, 23.0 * (3.0 * 8.0 + 11.0 * 10.0 + 13.0 * 7.0) / 25.0)
             incase(version, 'P00M', 1, 25, 12, 37, false, false, 23.0 * (3.0 * 8.0 + 11.0 * 10.0 + 13.0 * 7.0) / 25.0)
-            incase(version, 'PTRM', 1, 25, 12, 37, true, true, 23.0 * (3.0 * 8.0 + 11.0 * 5.0) / TYear)
+            incase(version, 'PTRM', 1, 25, 12, 37, true, true, 23.0 * (3.0 * 8.0 + 11.0 * 5.0) / DAYS_IN_YEAR)
             incase(version, 'P0RM', 1, 25, 12, 37, false, true, 23.0 * (3.0 * 8.0 + 11.0 * 5.0) / 25.0)
 
             incase(version, 'PT0F', 1, 35, 12, 47, true, false, 23.0 * (3.0 * 8.0 + 11.0 * 10.0 + 13.0 * 10.0 + 17.0 * 7.0) / 35.0)
             incase(version, 'P00F', 1, 35, 12, 47, false, false, 23.0 * (3.0 * 8.0 + 11.0 * 10.0 + 13.0 * 10.0 + 17.0 * 7.0) / 35.0)
-            incase(version, 'PTRF', 1, 35, 12, 47, true, true, 23.0 * (3.0 * 8.0 + 11.0 * 10.0 + 13.0 * 5.0) / TYear)
+            incase(version, 'PTRF', 1, 35, 12, 47, true, true, 23.0 * (3.0 * 8.0 + 11.0 * 10.0 + 13.0 * 5.0) / DAYS_IN_YEAR)
             incase(version, 'P0RF', 1, 35, 12, 47, false, true, 23.0 * (3.0 * 8.0 + 11.0 * 10.0 + 13.0 * 5.0) / 35.0)
 
             // cases relating to profile item wholly overlapping query range
             incase(version, 'LT0M', 12, 27, 1, 39, true, false, 23.0 * (2.0 * 9.0 + 3.0 * 10.0 + 11.0 * 10.0 + 13.0 * 9.0) / 38.0)
             incase(version, 'L00M', 12, 27, 1, 39, false, false, 23.0 * (2.0 * 9.0 + 3.0 * 10.0 + 11.0 * 10.0 + 13.0 * 9.0) / 38.0)
-            incase(version, 'LTRMD', 12, 27, 1, 39, true, true, 23.0 * (8.0 * 3.0 + 11.0 * 7.0) / TYear)
+            incase(version, 'LTRMD', 12, 27, 1, 39, true, true, 23.0 * (8.0 * 3.0 + 11.0 * 7.0) / DAYS_IN_YEAR)
             incase(version, 'L0RM', 12, 27, 1, 39, false, true, 23.0 * (8.0 * 3.0 + 11.0 * 7.0) / 38.0)
 
             incase(version, 'LT0F', 12, 37, 1, 49, true, false, 23.0 * (2.0 * 9.0 + 3.0 * 10.0 + 11.0 * 10.0 + 13.0 * 10.0 + 17.0 * 9.0) / 48.0)
             incase(version, 'L00F', 12, 37, 1, 49, false, false, 23.0 * (2.0 * 9.0 + 3.0 * 10.0 + 11.0 * 10.0 + 13.0 * 10.0 + 17.0 * 9.0) / 48.0)
-            incase(version, 'LTRFD', 12, 37, 1, 49, true, true, 23.0 * (8.0 * 3.0 + 11.0 * 10.0 + 13.0 * 7.0) / TYear)
+            incase(version, 'LTRFD', 12, 37, 1, 49, true, true, 23.0 * (8.0 * 3.0 + 11.0 * 10.0 + 13.0 * 7.0) / DAYS_IN_YEAR)
             incase(version, 'L0RF', 12, 37, 1, 49, false, true, 23.0 * (8.0 * 3.0 + 11.0 * 10.0 + 13.0 * 7.0) / 48.0)
 
             // cases relating to profile item without end partially overlapping range
             incase(version, 'ET0F', 1, 35, 12, null, true, false, 17.0 * 23.0)
             incase(version, 'E00F', 1, 35, 12, null, false, false, 17.0 * 23.0)
-            incase(version, 'ETRF', 1, 35, 12, null, true, true, 23.0 * (3.0 * 8.0 + 11.0 * 10.0 + 13.0 * 5.0) / TYear)
+            incase(version, 'ETRF', 1, 35, 12, null, true, true, 23.0 * (3.0 * 8.0 + 11.0 * 10.0 + 13.0 * 5.0) / DAYS_IN_YEAR)
             incase(version, 'E0RFD', 1, 35, 12, null, false, true, 23.0 * (3.0 * 8.0 + 11.0 * 10.0 + 13.0 * 5.0) / 23.0)
             // incase('E0RFD', 1, 35, 12, null, false, true, 0) // i (JH) disagree w spec
 
+        }
+    }
+
+    def incase(version, code, querystart, queryend, profilestart, profileend, perunit, prorata, objective, isseries = true) {
+
+        DateTimeFormatter dateFormat = ISODateTimeFormat.dateTimeNoMillis()
+
+        // check_calculation_range
+
+        // Create a profile item
+        def postParams = [name: code, 'values.energyPerTime': 23.0]
+        postParams.dataItemUid = getDataItemUid(isseries, perunit)
+        if (profilestart) {
+            def profileStartDate = DataItemService.EPOCH + profilestart
+            postParams.startDate = dateFormat.print(profileStartDate.getTime())
+        }
+        if (profileend) {
+            def profileEndDate = DataItemService.EPOCH + profileend
+            postParams.endDate = dateFormat.print(profileEndDate.getTime())
+        }
+
+        def responsePost = client.post(
+            path: "/${version}/profiles/UCP4SKANF6CS/items",
+            body: postParams,
+            requestContentType: URLENC,
+            contentType: JSON)
+        
+        assertEquals 201, responsePost.status
+        def location = responsePost.headers['Location'].value
+        def uid = location.split('/')[7]
+        assertNotNull uid
+        
+        // Fetch profile items and check values
+        def queryParams = [:]
+        if (querystart) {
+            def queryStartDate = DataItemService.EPOCH + querystart
+            queryParams.startDate = dateFormat.print(queryStartDate.getTime())
+        }
+        if (queryend) {
+            def queryEndDate = DataItemService.EPOCH + queryend
+            queryParams.endDate = dateFormat.print(queryEndDate.getTime())
+        }
+        if (prorata) {
+            queryParams.mode = 'prorata'
+        }
+        def responseGet = client.get(
+            path: "/${version}/profiles/UCP4SKANF6CS/items;name;amounts",
+            query: queryParams,
+            contentType: JSON)
+
+        assertEquals 200, responseGet.status
+
+        def item = responseGet.data.items.find { it.uid == uid }
+        assertEquals code, item.name
+
+        // Amounts
+        assertEquals 1, item.amounts.amount.size()
+        assertContainsAmount(item.amounts.amount, 'CO2', objective, 'kg', 'year', true)
+
+        // Delete the profile item
+        def responseDelete = client.delete(path: "/${version}/profiles/UCP4SKANF6CS/items/${uid}")
+        assertEquals 200, responseDelete.status
+
+        // Check it was deleted
+        // We should get a 404 here.
+        try {
+            client.get(path: "/${version}/profiles/UCP4SKANF6CS/items/${uid}")
+            fail 'Should have thrown an exception'
+        } catch (HttpResponseException e) {
+            assertEquals 404, e.response.status
+        }
+    }
+
+    def getDataItemUid(isSeries, hasPerUnit) {
+        if (isSeries) {
+            if (hasPerUnit) {
+                return '1F6B12D982B5'
+            } else {
+                return '58E2285C5310'
+            }
+        } else {
+            if (hasPerUnit) {
+                return 'A68029BBC709'
+            } else {
+                return '0BE050A60037'
+            }
         }
     }
 
@@ -754,89 +844,6 @@ class ProfileItemIT extends BaseApiTest {
             assertEquals isDefault, amount.default
         } else {
             assertNull amount.default
-        }
-    }
-
-    def incase(version, code, querystart, queryend, profilestart, profileend, perunit, prorata, objective, isseries = true) {
-
-        DateTimeFormatter dateFormat = ISODateTimeFormat.dateTimeNoMillis();
-
-        def name = code
-        def dataItemUid = getDataItemUid(isseries, perunit)
-        def profileStartDate = profilestart ? DataItemService.EPOCH + profilestart : null
-        def profileEndDate = profileend ? DataItemService.EPOCH + profileend : null
-        def queryStartDate = querystart ? DataItemService.EPOCH + querystart : null
-        def queryEndDate = queryend ? DataItemService.EPOCH + queryend : null
-
-        // check_calculation_range
-
-        // Create a profile item
-        def responsePost = client.post(
-            path: "/${version}/profiles/UCP4SKANF6CS/items",
-            body: [
-                name: name,
-                dataItemUid: dataItemUid,
-                startDate: dateFormat.print(profileStartDate?.getTime()),
-                endDate: dateFormat.print(profileEndDate?.getTime()),
-                'values.energyPerTime': 23.0],
-            requestContentType: URLENC,
-            contentType: JSON)
-        
-        assertEquals 201, responsePost.status
-        def location = responsePost.headers['Location'].value
-        def uid = location.split('/')[7]
-        assertNotNull uid
-        
-        // Fetch profile items and check values
-        def queryParams = [:]
-        if (queryStartDate) {
-            queryParams.startDate = dateFormat.print(queryStartDate?.getTime())
-        }
-        if (queryEndDate) {
-            queryParams.endDate = dateFormat.print(queryEndDate?.getTime())
-        }
-        if (prorata) {
-            queryParams.mode = 'prorata'
-        }
-        def responseGet = client.get(
-            path: "/${version}/profiles/UCP4SKANF6CS/items;name;amounts",
-            query: queryParams,
-            contentType: JSON)
-
-        assertEquals 200, responseGet.status
-
-        def item = responseGet.data.items.find { it.uid == uid }
-        assertEquals name, item.name
-
-        // Amounts
-        assertEquals 1, item.amounts.amount.size()
-        assertContainsAmount(item.amounts.amount, 'CO2', objective, 'kg', 'year', true)
-
-        // Delete the profile item
-        def responseDelete = client.delete(path: "/${version}/profiles/UCP4SKANF6CS/items/${uid}")
-        assertEquals 200, responseDelete.status
-
-        // Check it was deleted
-        // We should get a 404 here.
-        try {
-            client.get(path: "/${version}/profiles/UCP4SKANF6CS/items/${uid}")
-            fail 'Should have thrown an exception'
-        } catch (HttpResponseException e) {
-            assertEquals 404, e.response.status
-        }
-    }
-
-    def getDataItemUid(isSeries, hasPerUnit) {
-        if (isSeries) {
-            if (hasPerUnit) {
-                return '1F6B12D982B5'
-            }
-            return '58E2285C5310'
-        } else {
-            if (hasPerUnit) {
-                return 'A68029BBC709'
-            }
-            return '0BE050A60037'
         }
     }
 }
