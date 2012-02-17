670c8715e37944eaab471697562da4abc4f9ec92
conan <conan.cook@amee.com>
2012-02-17 14:52:12 +0000

    change json output to use appropriate datatypes PL-11209

--- src/main/java/com/amee/platform/resource/itemvaluedefinition/v_3_4/ItemValueDefinitionJSONRenderer_3_4_0.java
+++ src/main/java/com/amee/platform/resource/itemvaluedefinition/v_3_4/ItemValueDefinitionJSONRenderer_3_4_0.java
@@ -61,7 +61,26 @@ public class ItemValueDefinitionJSONRenderer_3_4_0 implements ItemValueDefinitio
 
     @Override
     public void addValue() {
-        ResponseHelper.put(itemValueDefinitionObj, "value", itemValueDefinition.getValue());
+    	// JSON supports boolean, number and text types (it supports more, but these are relevant here)
+    	ValueType type = itemValueDefinition.getValueDefinition().getValueType();
+    	switch(type){
+    	case BOOLEAN:
+    		Boolean booleanValue = Boolean.valueOf(itemValueDefinition.getValue());
+    		ResponseHelper.put(itemValueDefinitionObj, "value", booleanValue);
+    		break;
+    	case DOUBLE:
+    		Double doubleValue = Double.valueOf(itemValueDefinition.getValue());
+    		ResponseHelper.put(itemValueDefinitionObj, "value", doubleValue);
+    		break;
+    	case INTEGER:
+    		Integer integerValue = Integer.valueOf(itemValueDefinition.getValue());
+    		ResponseHelper.put(itemValueDefinitionObj, "value", integerValue);
+    		break;
+    	// Our default is to use a String
+    	default:
+    		ResponseHelper.put(itemValueDefinitionObj, "value", itemValueDefinition.getValue());
+    		break;
+    	}
     }
 
     @Override
