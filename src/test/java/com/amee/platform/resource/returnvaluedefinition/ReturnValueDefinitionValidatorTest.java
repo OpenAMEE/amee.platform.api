d0fa0e488a75ec83fa2c710d3a2cef0c7f12f745
conan <conan.cook@amee.com>
2012-02-21 10:16:22 +0000

    add unit tests for new name field in ReturnValueDefinition PL-11017

--- src/test/java/com/amee/platform/resource/returnvaluedefinition/ReturnValueDefinitionValidatorTest.java
+++ src/test/java/com/amee/platform/resource/returnvaluedefinition/ReturnValueDefinitionValidatorTest.java
@@ -13,96 +13,130 @@ import static org.junit.Assert.assertTrue;
 
 public class ReturnValueDefinitionValidatorTest {
 
-    @Test
-    public void testValid() {
-        ReturnValueDefinitionValidator_3_1_0 validator = new ReturnValueDefinitionValidator_3_1_0();
-        ReturnValueDefinition good = new ReturnValueDefinition();
-        BindException errorsGood = new BindException(good, "good");
+	@Test
+	public void testValid() {
+		ReturnValueDefinitionValidator_3_1_0 validator = new ReturnValueDefinitionValidator_3_1_0();
+		ReturnValueDefinition good = new ReturnValueDefinition();
+		BindException errorsGood = new BindException(good, "good");
 
-        good.setType("CO2");
-        good.setUnit(AmountUnit.valueOf("kg"));
-        good.setPerUnit(AmountPerUnit.valueOf("month"));
-        good.setDefaultType(true);
+		good.setType("CO2");
+		good.setUnit(AmountUnit.valueOf("kg"));
+		good.setPerUnit(AmountPerUnit.valueOf("month"));
+		good.setDefaultType(true);
+		good.setName("kg CO2 per month");
 
-        validator.setObject(good);
-        validator.initialise();
-        validator.validate(good, errorsGood);
-        assertFalse("Object should not fail validation: (" + errorsGood.getMessage() + ")", errorsGood.hasErrors());
-    }
+		validator.setObject(good);
+		validator.initialise();
+		validator.validate(good, errorsGood);
+		assertFalse("Object should not fail validation: (" + errorsGood.getMessage() + ")", errorsGood.hasErrors());
+	}
 
-    @Test
-    public void testTypeGreaterThanMax() {
-        ReturnValueDefinitionValidator_3_1_0 validator = new ReturnValueDefinitionValidator_3_1_0();
-        ReturnValueDefinition bad = new ReturnValueDefinition();
-        BindException errorsBad = new BindException(bad, "bad");
+	@Test
+	public void testTypeGreaterThanMax() {
+		ReturnValueDefinitionValidator_3_1_0 validator = new ReturnValueDefinitionValidator_3_1_0();
+		ReturnValueDefinition bad = new ReturnValueDefinition();
+		BindException errorsBad = new BindException(bad, "bad");
 
-        String typeGreaterThanMax = RandomStringUtils.random(ReturnValueDefinition.TYPE_MAX_SIZE + 1);
-        bad.setType(typeGreaterThanMax);
+		String typeGreaterThanMax = RandomStringUtils.random(ReturnValueDefinition.TYPE_MAX_SIZE + 1);
+		bad.setType(typeGreaterThanMax);
 
-        validator.setObject(bad);
-        validator.initialise();
-        validator.validate(bad, errorsBad);
-        assertTrue("Object should fail validation", errorsBad.hasErrors());
-        assertTrue("'type' field should be in error", errorsBad.hasFieldErrors("type"));
-    }
+		validator.setObject(bad);
+		validator.initialise();
+		validator.validate(bad, errorsBad);
+		assertTrue("Object should fail validation", errorsBad.hasErrors());
+		assertTrue("'type' field should be in error", errorsBad.hasFieldErrors("type"));
+	}
 
-    @Test
-    public void testEmptyType() {
-        ReturnValueDefinitionValidator_3_1_0 validator = new ReturnValueDefinitionValidator_3_1_0();
-        ReturnValueDefinition bad = new ReturnValueDefinition();
-        BindException errorsBad = new BindException(bad, "bad");
+	@Test
+	public void testEmptyType() {
+		ReturnValueDefinitionValidator_3_1_0 validator = new ReturnValueDefinitionValidator_3_1_0();
+		ReturnValueDefinition bad = new ReturnValueDefinition();
+		BindException errorsBad = new BindException(bad, "bad");
 
-        bad.setType("");
+		bad.setType("");
 
-        validator.setObject(bad);
-        validator.initialise();
-        validator.validate(bad, errorsBad);
-        assertTrue("Object should fail validation", errorsBad.hasErrors());
-        assertTrue("'type' field should be in error", errorsBad.hasFieldErrors("type"));
-    }
+		validator.setObject(bad);
+		validator.initialise();
+		validator.validate(bad, errorsBad);
+		assertTrue("Object should fail validation", errorsBad.hasErrors());
+		assertTrue("'type' field should be in error", errorsBad.hasFieldErrors("type"));
+	}
 
-    @Test
-    public void testTypeBadChars() {
-        ReturnValueDefinitionValidator_3_1_0 validator = new ReturnValueDefinitionValidator_3_1_0();
-        ReturnValueDefinition bad = new ReturnValueDefinition();
-        BindException errorsBad = new BindException(bad, "bad");
+	@Test
+	public void testNameGreaterThanMax() {
+		ReturnValueDefinitionValidator_3_1_0 validator = new ReturnValueDefinitionValidator_3_1_0();
+		ReturnValueDefinition bad = new ReturnValueDefinition();
+		BindException errorsBad = new BindException(bad, "bad");
 
-        bad.setType("!!!!");
+		bad.setType("CO2");
+		String nameGreaterThanMax = RandomStringUtils.random(ReturnValueDefinition.TYPE_MAX_SIZE + 1);
+		bad.setName(nameGreaterThanMax);
 
-        validator.setObject(bad);
-        validator.initialise();
-        validator.validate(bad, errorsBad);
-        assertTrue("Object should fail validation", errorsBad.hasErrors());
-        assertTrue("'type' field should be in error", errorsBad.hasFieldErrors("type"));
-    }
+		validator.setObject(bad);
+		validator.initialise();
+		validator.validate(bad, errorsBad);
+		assertTrue("Object should fail validation", errorsBad.hasErrors());
+		assertTrue("'name' field should be in error", errorsBad.hasFieldErrors("name"));
+	}
 
-    @Test(expected = IllegalArgumentException.class)
-    public void testInvalidUnit() {
-        ReturnValueDefinitionValidator_3_1_0 validator = new ReturnValueDefinitionValidator_3_1_0();
-        ReturnValueDefinition bad = new ReturnValueDefinition();
-        BindException errorsBad = new BindException(bad, "bad");
+	@Test
+	public void testEmptyName() {
+		ReturnValueDefinitionValidator_3_1_0 validator = new ReturnValueDefinitionValidator_3_1_0();
+		ReturnValueDefinition bad = new ReturnValueDefinition();
+		BindException errorsBad = new BindException(bad, "bad");
 
-        bad.setType("CO2");
-        bad.setUnit(AmountUnit.valueOf("NOT_A_UNIT"));
-        bad.setPerUnit(AmountPerUnit.valueOf("month"));
+		bad.setType("CO2");
+		bad.setName("");
 
-        validator.setObject(bad);
-        validator.initialise();
-        validator.validate(bad, errorsBad);
-    }
+		validator.setObject(bad);
+		validator.initialise();
+		validator.validate(bad, errorsBad);
+		assertTrue("Object should fail validation", errorsBad.hasErrors());
+		assertTrue("'name' field should be in error", errorsBad.hasFieldErrors("name"));
+	}
 
-    @Test(expected = IllegalArgumentException.class)
-    public void testInvalidPerUnit() {
-        ReturnValueDefinitionValidator_3_1_0 validator = new ReturnValueDefinitionValidator_3_1_0();
-        ReturnValueDefinition bad = new ReturnValueDefinition();
-        BindException errorsBad = new BindException(bad, "bad");
+	@Test
+	public void testTypeBadChars() {
+		ReturnValueDefinitionValidator_3_1_0 validator = new ReturnValueDefinitionValidator_3_1_0();
+		ReturnValueDefinition bad = new ReturnValueDefinition();
+		BindException errorsBad = new BindException(bad, "bad");
 
-        bad.setType("CO2");
-        bad.setUnit(AmountUnit.valueOf("kg"));
-        bad.setPerUnit(AmountPerUnit.valueOf("NOT_A_PER_UNIT"));
+		bad.setType("!!!!");
 
-        validator.setObject(bad);
-        validator.initialise();
-        validator.validate(bad, errorsBad);
-    }
+		validator.setObject(bad);
+		validator.initialise();
+		validator.validate(bad, errorsBad);
+		assertTrue("Object should fail validation", errorsBad.hasErrors());
+		assertTrue("'type' field should be in error", errorsBad.hasFieldErrors("type"));
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testInvalidUnit() {
+		ReturnValueDefinitionValidator_3_1_0 validator = new ReturnValueDefinitionValidator_3_1_0();
+		ReturnValueDefinition bad = new ReturnValueDefinition();
+		BindException errorsBad = new BindException(bad, "bad");
+
+		bad.setType("CO2");
+		bad.setUnit(AmountUnit.valueOf("NOT_A_UNIT"));
+		bad.setPerUnit(AmountPerUnit.valueOf("month"));
+
+		validator.setObject(bad);
+		validator.initialise();
+		validator.validate(bad, errorsBad);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testInvalidPerUnit() {
+		ReturnValueDefinitionValidator_3_1_0 validator = new ReturnValueDefinitionValidator_3_1_0();
+		ReturnValueDefinition bad = new ReturnValueDefinition();
+		BindException errorsBad = new BindException(bad, "bad");
+
+		bad.setType("CO2");
+		bad.setUnit(AmountUnit.valueOf("kg"));
+		bad.setPerUnit(AmountPerUnit.valueOf("NOT_A_PER_UNIT"));
+
+		validator.setObject(bad);
+		validator.initialise();
+		validator.validate(bad, errorsBad);
+	}
 }
