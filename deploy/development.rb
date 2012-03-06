30136738f60ce6ee7e2b80126de328167befb456
David Keen <david.keen@amee.com>
2012-03-06 11:11:15 +0000

    Update deploy script to point to app3. PL-11271.

--- deploy/development.rb
+++ deploy/development.rb
@@ -1,6 +1,5 @@
 # The hosts where are are deploying 
-role :app, "monsoon.amee.com"
-role :db,  "monsoon.amee.com", :primary => true
+role :app, "app3.amee.com"
 
 set :application, "platform-api-dev"
 set :deploy_to, "/var/www/apps/#{application}"
