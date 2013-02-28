5a7731dc897a31cda06d7f488ee0a494625b123d
conan <conan.cook@amee.com>
2013-02-28 11:08:11 +0000

    update ec2 hostnames for production deployment for new platform boxes

--- deploy/production.rb
+++ deploy/production.rb
@@ -1,5 +1,5 @@
 # The hosts where are are deploying 
-role :app, "ec2-23-22-0-248.compute-1.amazonaws.com", "ec2-54-242-0-90.compute-1.amazonaws.com"
+role :app, "ec2-54-242-111-35.compute-1.amazonaws.com", "ec2-184-73-122-24.compute-1.amazonaws.com"
 
 set :application, "v3.amee.com"
 set :deploy_to, "/var/www/apps/#{application}"
